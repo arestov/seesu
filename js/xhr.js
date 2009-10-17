b2774d3e97415abc3c4d8f984bf7e0595845c623
U-51A\Gleb <Gleb@51a.(none)>
2009-10-17 12:34:47 +0400

    vkontakte draft login works

--- js/xhr.js
+++ js/xhr.js
@@ -1,9 +1,26 @@
 // XHR example
+
+var loginxhr = new XMLHttpRequest ();
+loginxhr.onreadystatechange = function(){
+  if (this.readyState == 4){
+  	document.body.style.background = 'red';
+  	opera.postError('XHR logging in');
+  	opera.postError(xhr.responseText);
+  	xhr.send(param);//запрос музыки
+  }
+}
+loginxhr.open('POST', 'http://vkontakte.ru/login.php');
+loginxhr.xhrparams = 'noredirect=1';
+loginxhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+loginxhr.setRequestHeader("host", "vkontakte.ru");
+
+
+
 var xhr = new XMLHttpRequest ();
 xhr.onreadystatechange = function () {
   if ( this.readyState == 4 ) {
     document.body.style.background = '#CEC';
-    opera.postError('XHR working!');
+    opera.postError('XHR music!');
     opera.postError(xhr.responseText);
   }
 }
@@ -13,5 +30,12 @@ xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
 window.onload = function(){
-  xhr.send(param);
+  //логин
+  $('#enter-vk').click(function(){
+  	loginxhr.xhrparams += '&email=' + encodeURIComponent($('#login-vk')[0].value) + '&pass=' + encodeURIComponent($('#pass-vk')[0].value);
+  	loginxhr.send(loginxhr.xhrparams); 
+  })  
+
+
 }
+
