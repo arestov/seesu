d4f9c4ac5ccfd8b2c2f9e77a1e063d9c2a95bab5
Nikita Vasilyev <me@elv1s.ru>
2009-10-17 13:31:35 +0400

    Opera wand now remember your password Added output display for login and music search

--- js/xhr.js
+++ js/xhr.js
@@ -2,18 +2,16 @@
 
 var loginxhr = new XMLHttpRequest ();
 loginxhr.onreadystatechange = function(){
-  if (this.readyState == 4){
-  	document.body.style.background = 'red';
-  	opera.postError('XHR logging in');
-  	opera.postError(xhr.responseText);
-  	xhr.send(param);//запрос музыки
+  if (this.readyState == 4) {
+    document.body.style.background = 'red';
+    document.getElementById('auth').innerHTML += loginxhr.responseText;
+    xhr.send(param);//запрос музыки
   }
-}
+};
 loginxhr.open('POST', 'http://vkontakte.ru/login.php');
 loginxhr.xhrparams = 'noredirect=1';
 loginxhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 loginxhr.setRequestHeader("host", "vkontakte.ru");
-
 
 
 var xhr = new XMLHttpRequest ();
@@ -21,9 +19,9 @@ xhr.onreadystatechange = function () {
   if ( this.readyState == 4 ) {
     document.body.style.background = '#CEC';
     opera.postError('XHR music!');
-    opera.postError(xhr.responseText);
+    document.getElementById('search_result').innerHTML = xhr.responseText;
   }
-}
+};
 xhr.open( 'POST', 'http://vkontakte.ru/gsearch.php', false );
 var param = 'c[q]=The Prodigy&c[section]=audio';
 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -31,11 +29,11 @@ xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
 window.onload = function(){
   //логин
-  $('#enter-vk').click(function(){
-  	loginxhr.xhrparams += '&email=' + encodeURIComponent($('#login-vk')[0].value) + '&pass=' + encodeURIComponent($('#pass-vk')[0].value);
-  	loginxhr.send(loginxhr.xhrparams); 
-  })  
+  document.getElementById('auth').onsubmit = function(){
+    loginxhr.xhrparams += '&email=' + encodeURIComponent($('#login')[0].value) + '&pass=' + encodeURIComponent($('#pass')[0].value);
+    loginxhr.send(loginxhr.xhrparams);
+    return false;
+  };
 
-
-}
+};
 
