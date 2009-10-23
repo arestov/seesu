3b9b8c73ad0c910cfbc0c48d400b728a2a8efcaf
Aleksandr Koss <koss@octolab.ru>
2009-10-24 04:08:18 +0700

    Player events manager

--- js/vk_player.js
+++ js/vk_player.js
@@ -1,136 +1,157 @@
- function player_DoFSCommand(c, args) {
+const INIT     = -11,
+      CREATED  = -7,
+      VOLUME   = -5,
+      STOPPED  =  1,
+      PLAYED   =  5,
+      PAUSED   =  7,
+      FINISHED =  11;
 
-    if(args.match('playing')) alert('играю йоу');
-    if(args.match('paused')) alert('пауза');
-    if(args.match('finished')) alert('кончилось');
-alert('тыдыщь!');
-    /*flashDebug(0, false, "flashvars,debug,false", "undefined");
-    
-    flashDebug(66027115, false, "flashvars,dbid,66027115", "undefined");
-    
-    flashDebug(66027115, false, "flashvars,url,http://cs4507.vkontakte.ru/u18157945/audio/d8b60a4e9ba1.mp3", "undefined");
-    
-    stateChanged(66027115, false, "playing", "id=no_file,debugInfo=[empty]");
-    
-    stateChanged(66027115, false, "init", "id=no_file,debugInfo=[empty]");
-    
-    stateChanged(66027115, false, "created", "id=no_file,debugInfo=[empty]");
-    
-    // Измненена громкость
-    
-    stateChanged(66027115, false, "volume", "47.1428571428571");
-    
-    stateChanged(66027115, false, "paused", "id=no_file,debugInfo=[empty]");
-    stateChanged(66027115, false, "finished", "id=no_file,debugInfo=[empty]");
-  */}
+var player_state = STOPPED,
+    song_url = '',
+    player_holder = null,
 
-  function set_var(func, value){
+    holy_vk_string = 
+      '<embed width="342" height="14" ' + 
+      'flashvars="debug=false&amp;volume=:volume&amp;' +
+      'url=:url" allowscriptaccess="always" wmode="transparent" swliveconnect="true" quality="high" ' +
+      'bgcolor=":background_color" name="player" id="player" style="" ' +
+      'src="http://vkontakte.ru/swf/AudioPlayer_mini.swf?0.9.9" ' +
+      'type="application/x-shockwave-flash"/>',
+      
+    start_volume = widget.preferenceForKey('vkplayer-volume') || 80,
+     
+    background_color = '#FFFFFF',
+    events = new Array;    
+
+// VK player actions
+
+function call_event(event, data) {
+  log('Event: ' + event);
+  if(events[event]) events[event](data);
+}
+
+function player_DoFSCommand(c, args) {
+
+  if(args.match('playing')) call_event(PLAYED);
   
-		var player = window.document.player;//$('.played-song embed').get();
-		var counter = 0;
-		var interval = 0;
-		var cycle = function(){
-			try {
-				if (player.SetVariable) {
-					player.SetVariable("audioPlayer_mc." + func, value);
-					clearInterval(interval);
-					return true;
-				}
-			}
-			catch (e) {
-				//alert(e.message)
-			}
+  if(args.match('paused')) call_event(PAUSED);
+  
+  if(args.match('finished')) call_event(FINISHED);
+  
+  if(args.match('init')) call_event(INIT);
+  
+  if(args.match('created')) call_event(CREATED);
 
-			if (counter++ > 3) {
-				clearInterval(interval);
-			}
-			return false;
-		};
-		if (!cycle()) {
-			interval = setInterval(cycle, 50);
-		}
-	}
+  if(args.match('volume')) call_event(VOLUME/*, ??? */);
+}
+
+function set_var(variable, value) {
+	window.document.player.SetVariable("audioPlayer_mc." + variable, value);
+}  
+
+function create_player() {
+	player_holder.html(
+		holy_vk_string
+		  .replace(':url', song_url)
+		  .replace(':volume', start_volume)
+		  .replace(':background_color', background_color)
+	);
 	
-$(function() {
- var playingNode;
- var start_volume = widget.preferenceForKey('vkplayer-volume') || 80;
- var playholder = $('#player-holder');
- $(document).click(function(e){
- 	var node = e.target,
- 	nodeClass = node.className;
- 	if (node.nodeName == 'A') {
-		if (nodeClass.indexOf('song') != -1) {
-	 		var song_url = node.getAttribute('href');
-	 		$(playingNode).removeClass('active-play');
-			playholder.html(
-				holy_vk_string
-				  .replace(':url', song_url)
-				  .replace(':volume', start_volume)
-				  .replace(':background_color', background_color)
-			)
-			$(node).addClass('active-play');
-			playingNode = node;
-			return false
-	 	} else 
-		if (nodeClass.indexOf('vk-reg-ref') != -1) {
-			widget.openURL(vkReferer);
-			return false
-		}
-	}
- });
- var holy_vk_string = 
-   '<embed width="342" height="14" ' + 
-   'flashvars="debug=false&amp;volume=:volume&amp;' +
-	 'url=:url" allowscriptaccess="always" wmode="transparent" swliveconnect="true" quality="high" ' +
-	 'bgcolor=":background_color" name="player" id="player" style="" ' +
-	 'src="http://vkontakte.ru/swf/AudioPlayer_mini.swf?0.9.9" ' +
-	 'type="application/x-shockwave-flash"/>',
-	 
-	 background_color = '#FFFFFF';
- /*
-  $('a.song').live('click', function() {
-    var song_url = $(this).attr('href');
-    
-        
-    playholder.html(
-    	holy_vk_string
-        .replace(':url', song_url)
-        .replace(':volume', start_volume)
-        .replace(':background_color', background_color)
-    )    
-        /*
-    $('.played-song').each(function() {
-      $(this).parent().html($(this).find('span').html());
-        //.remove();
-    });
+	player_state = PLAYED;
+}
 
-    $(this).wrap(
-      '<div class="played-song"><span></span>' +
-      holy_vk_string
-        .replace(':url', song_url)
-        .replace(':volume', start_volume)
-        .replace(':id', id)
-        .replace(':background_color', background_color) +
-      '</div>'
+// Player actions
+
+function play_pause() {
+  set_var('buttonPressed', 'true');
+}
+
+function play() {
+  log('Играй гормонь!')
+  play_pause();
+  player_state = PLAYED;
+}
+
+
+function stop() {
+  log('Стой сука');
+  set_var('setState', 'stop');
+  player_state = STOPPED;
+}
+
+
+function pause() {
+  log('Паузе')
+  play_pause();
+  player_state = PAUSED;
+}
+
+// Player state switcher
+
+function set_state(new_player_state_str) {
+
+  var new_player_state =
+    (new_player_state_str == "play" ? PLAYED :
+      (new_player_state_str == "stop" ? STOPPED : PAUSED)
     );
-    return false;
+
+  log('Old state: ' + player_state);
+  log('Set state! ' + new_player_state);
+
+  log(player_state - new_player_state);
+
+  switch(player_state - new_player_state) {
+  case(STOPPED - PLAYED):
+    create_player();
+    break;
+  case(PAUSED - PLAYED):
+    play();
+    break;    
+  case(PAUSED - STOPPED):
+  case(PLAYED - STOPPED):
+    stop();
+    break;
+  case(PLAYED - PAUSED):
+    pause();
+    break;
+  default:
+    log('Do nothing');
+    // Do nothing
+  }
+}
+
+// Ready? Steady? Go!
+
+$(function() {
+  player_holder = $('#player-holder');
+
+  var playingNode;
+
+  $(document).click(function(e) {
+  	var node = e.target,
+  	    nodeClass = node.className;
+  	
+  	if(node.nodeName == 'A') {
+  	if(nodeClass.indexOf('song') != -1) {
+   		
+   		song_url = node.getAttribute('href');
+   	
+   		$(playingNode).removeClass('active-play');
+  		
+  		create_player();
+  		
+  		$(node).addClass('active-play');
+  		playingNode = node;
+  		return false
+   	} else if(nodeClass.indexOf('vk-reg-ref') != -1) {
+  		
+  		widget.openURL(vkReferer);
+  		return false
+  	}
+  }
   });
-  */
-  
-  $('#stop').click(function() {
-	  set_var("setState", "stop");
-	  //  window.document.SetVariable("setState", "stop", false);
-	   // $('.played-song embed').SetVariable("setState", "stop", false);//("audioPlayer_mc." + func, value);
-	    return false;
-  });
-  /*
-	$('#pause').click(function() {
-		set_var("setState", "pause");
-		return false;
-	});
-	$('#play').click(function() {
-		set_var("setState", "play");
-		return false;
-	});*/
-  // VK player events handler
+
+  $('#stop, #pause, #play').click(
+    function() { set_state($(this).attr('id')); return false; }
+  );
 });
