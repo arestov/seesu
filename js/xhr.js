ccb6409aa8e9fc4dc1fd14427ba7ae1eb41f6f63
U-51A\Gleb <Gleb@51a.(none)>
2009-10-25 13:17:38 +0300

    jslint

--- js/xhr.js
+++ js/xhr.js
@@ -23,7 +23,7 @@ updatex.onreadystatechange = function(){
 		if (link.indexOf('http') != -1) {
 			widget.showNotification(message, function(){
 				widget.openURL(link);
-			})
+			});
 		}
 
 	}
@@ -51,32 +51,32 @@ var vk_logg_in = function(id,email){
 	widget.setPreferenceForKey(email, 'vkemail');
 	vk_logged_in = true;
 	$(document.body).addClass('vk-logged-in');
-	log('вошли в контакте и скрыли форму логина')
-}
+	log('вошли в контакте и скрыли форму логина');
+};
 var vk_logged_out = function(){
 	widget.setPreferenceForKey(null, 'vkid');
 	widget.setPreferenceForKey(null, 'vkemail');
 	vk_logged_in = false;
 	$(document.body).removeClass('vk-logged-in');
-	log('отображаем форму логина где нужно')
+	log('отображаем форму логина где нужно');
 	
-}
+};
 
 var loginxhr = new XMLHttpRequest ();
 loginxhr.onreadystatechange = function(){
   if (this.readyState == 4) {
-  	log(loginxhr.responseText);
+	log(loginxhr.responseText);
 	if ((loginxhr.responseText.indexOf('id') != -1) && 
 		(loginxhr.responseText.indexOf('email') != -1) && 
 		(loginxhr.responseText.indexOf('sid') != -1) && 
 		(loginxhr.responseText.indexOf('pass') != -1)  ) {
 		var r = JSON.parse(loginxhr.responseText);
 		if (r.id) {
-			log(vk_logged_in)
+			log(vk_logged_in);
 			vk_logg_in(r.id, r.email);
 			wait_for_vklogin && wait_for_vklogin();
 		}	
-	} else log('не получается войти')
+	} else {log('не получается войти');}
   }
 };
 loginxhr.open('POST', 'http://vkontakte.ru/login.php');
@@ -92,17 +92,17 @@ var parseStrToObj = function(onclickstring){
 	var params = b.split(','),
 		server = params[1],
 		user = params[2];
-	while (user.length < 5) user = '0' + user;
+	while (user.length < 5) {user = '0' + user;}
 	fname = params[3];
 	fname = fname.substring(1, fname.length - 1);
 	var obj ={'sever': server, 'user' : user , 'filename' : fname, 'link' : ('http://cs' + server + '.vkontakte.ru/u' + user + '/audio/' + fname + '.mp3')};
 	return obj;
 
-}
+};
 
 
 var getMusic = function(trackname){
-	if (!vk_logged_in) return false
+	if (!vk_logged_in) {return false;}
 	
 	var musicList = [];
 	musicList.links = [];
@@ -111,7 +111,7 @@ var getMusic = function(trackname){
 	
 	xhr.onreadystatechange = function () {
 	  if ( this.readyState == 4 ) {
-	  	log(xhr.responseText);
+		log(xhr.responseText);
 		if (xhr.responseText.indexOf('rows') != -1) {
 			var srd = document.createElement('div');
 			srd.innerHTML = JSON.parse(xhr.responseText).rows;
@@ -130,14 +130,14 @@ var getMusic = function(trackname){
 				obj.track = track;
 				
 				musicList.push(obj);
-			};
+			}
 		} else {
 			log('Поиск не удался... :’—(');
 			if ((xhr.responseText.indexOf('http://vkontakte.ru/login.php?op=logout') != -1) && xhr.responseText.indexOf('http://vkontakte.ru/images/progress.gif' != -1)) {
 				vk_logged_out();
-				log('квантакте изгнал вас из рая')
+				log('квантакте изгнал вас из рая');
 			}
-			return false
+			return false;
 		}
 	  }
 	};
@@ -147,73 +147,76 @@ var getMusic = function(trackname){
 	xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 	xhr.send(param);
 		
-	return musicList
-}
+	return musicList;
+};
 var get_vk_track = function(trackname,tracknode,playlist_nodes_for) {
-	if (!vk_logged_in) return false
-	var now = (new Date()).getTime(),
-		timeout;
-	
-	arguments.callee.call_at = arguments.callee.call_at || now;
-	if ( arguments.callee.call_at && (arguments.callee.call_at > now)) {
-		timeout = arguments.callee.call_at - now;
+	if (!vk_logged_in) {
+		return false;
 	} else {
-		timeout = 0;
-		arguments.callee.call_at = now;
+		var now = (new Date()).getTime(),
+			timeout;
+		
+		arguments.callee.call_at = arguments.callee.call_at || now;
+		if ( arguments.callee.call_at && (arguments.callee.call_at > now)) {
+			timeout = arguments.callee.call_at - now;
+		} else {
+			timeout = 0;
+			arguments.callee.call_at = now;
+		}
+		
+		setTimeout(function(){
+			$.ajax({
+			  url: "http://vkontakte.ru/gsearch.php",
+			  global: false,
+			  type: "POST",
+			  data: ({'c[section]' : 'audio', 'c[q]' : trackname}),
+			  dataType: "json",
+			  beforeSend: function(){
+				tracknode.addClass('search-mp3');
+			  },
+			  error: function(){
+				tracknode.attr('class' , 'search-mp3-failed');
+			  },
+			  success: function(r){
+				log('Квантакте говорит: ' + r.summary);
+				var srd = document.createElement('div');
+					srd.innerHTML = r.rows;
+				var rows = $(".audioRow ", srd);
+				if (rows.length) {
+					var row = rows[0],
+						playStr = $('img.playimg', row )[0].getAttribute('onclick'),
+						link = parseStrToObj(playStr).link;
+					make_node_playable(tracknode,link,playlist_nodes_for);
+				} else {
+					tracknode.attr('class' , 'search-mp3-failed');
+				}
+				
+			  }
+			});
+		},timeout);
+		
+		arguments.callee.call_at += 900;
 	}
 	
-	setTimeout(function(){
-		$.ajax({
-		  url: "http://vkontakte.ru/gsearch.php",
-		  global: false,
-		  type: "POST",
-		  data: ({'c[section]' : 'audio', 'c[q]' : trackname}),
-		  dataType: "json",
-		  beforeSend: function(){
-		  	tracknode.addClass('search-mp3');
-		  },
-		  error: function(){
-		  	tracknode.attr('class' , 'search-mp3-failed');
-	 	  },
-		  success: function(r){
-		  	log('Квантакте говорит: ' + r.summary);
-			var srd = document.createElement('div');
-				srd.innerHTML = r.rows;
-			var rows = $(".audioRow ", srd);
-			if (rows.length) {
-				var row = rows[0],
-					playStr = $('img.playimg', row )[0].getAttribute('onclick'),
-					link = parseStrToObj(playStr).link;
-				make_node_playable(tracknode,link,playlist_nodes_for);
-			} else {
-				tracknode.attr('class' , 'search-mp3-failed');
-			}
-			
-		  }
-		});
-	},timeout)
 	
-	arguments.callee.call_at += 900;
-	
-	return
-}
+	return false;
+};
 
 var make_tracklist_playable = function(playlist,track_nodes){
 	if (vk_logged_in) {
-		var songNodes = [],
-			timeout = 0;
+		var songNodes = [];
 		for (var i=0, l =  playlist.length; i < l; i++) {
 			var node = track_nodes[i],
 				trackname = playlist[i],
 				playlist_nodes_for = songNodes;
 			get_vk_track(trackname,node,playlist_nodes_for);
-		};
+		}
 	} else {
 		wait_for_vklogin = function(){
 			make_tracklist_playable(playlist,track_nodes);
-		}
+		};
 	}
-}
+};
 var make_node_playable = function(node,http_link,playlist_nodes_for){
 	var playable_node = $(node).attr({'class' : 'song', 'href' : http_link} );
 	playlist_nodes_for.push(playable_node);
@@ -230,7 +233,7 @@ var make_node_playable = function(node,http_link,playlist_nodes_for){
 
 	var mp3 = $("<a></a>").attr({ 'class': 'download-mp3', 'text': 'mp3', 'href': http_link });
 	playable_node.parent().append(mp3);
-}
+};
 
 var prerenderPlaylist = function(playlist,container,mp3links) { // if links present than do full rendering! yearh!
 	var linkNodes = [];
@@ -245,31 +248,31 @@ var prerenderPlaylist = function(playlist,container,mp3links) { // if links pres
 		$(li).append(track);
 		if (mp3links) {
 			var link = mp3links[i];
-			make_node_playable(track,link,songNodes)
-		};
+			make_node_playable(track,link,songNodes);
+		}
 		$(ul).append(li);		
 		linkNodes.push(track);
-	};
+	}
 	(container && container.html('').append(ul)) || ($(searchres).html('').append(ul) && mp3links && (slider.className = 'screen-search'));
-	return linkNodes
+	return linkNodes;
 	
-}
+};
 var getTopTracks = function(artist) {
 	var tracks = lastfm('artist.getTopTracks',{'artist': artist }).toptracks.track || false;
 	if (tracks) {
 		var playlist = [];
 		for (var i=0, l = (tracks.length < 15) ? tracks.length : 15; i < l; i++) {
 			playlist.push(artist + ' - ' + tracks[i].name);
-		};
-		return playlist
+		}
+		return playlist;
 		
-	} else return false
+	} else {return false;}
 }
 
 var setArtistPage = function (artist,image) {
 	slider.className = 'sreen-artist-page';
 	player_holder = artsplhld;
-	if (nav_artist_page.textContent == artist) return true;
+	if (nav_artist_page.textContent == artist) {return true;}
 	nav_artist_page.innerHTML = artist;
 	var bio = lastfm('artist.getInfo',{'artist': artist }).artist.bio.summary;
 	artsName.text(artist);
@@ -283,7 +286,7 @@ var setArtistPage = function (artist,image) {
 	
 	
 	
-}
+};
 var artistsearch = function(artist) {
 	
 	var artists = lastfm('artist.search',{artist: artist, limit: 10 }).results.artistmatches.artist || false; 
@@ -294,19 +297,17 @@ var artistsearch = function(artist) {
 		
 		
 		searchres.innerHTML = '';
-		var ul = $("<ul></ul>").attr({ class: 'results-artists'});
+		var ul = $("<ul></ul>").attr({ 'class': 'results-artists'});
 		$(searchres).append(ul);
 		for (var i=0; i < artists.length; i++) {
 			var artist = artists[i].name;
 			var image = artists[i].image[1]['#text'] || 'http://cdn.last.fm/flatness/catalogue/noimage/2/default_artist_medium.png';
 			var li = $("<li></li>").data('artist',artist);
-			li.data('img', image)
+			li.data('img', image);
 			$(li).click(function(){
 				var artist = $(this).data('artist');
 				var image = $(this).data('img');
 				setArtistPage(artist,image);
-
-			
 			});
 			var p = $("<p></p>").attr({ text: artist});
 			if(image){
@@ -316,7 +317,7 @@ var artistsearch = function(artist) {
 			
 			$(li).append(p);
 			$(ul).append(li);
-		};
+		}
 		
 	} else {
 		searchres.innerHTML = '';
@@ -327,40 +328,39 @@ var artistsearch = function(artist) {
 		$(searchres).append(p);
 		slider.className = "screen-search";
 	}
-}
+};
 
 
 window.addEventListener( 'load' , function(){
 	$('#close-widget').click(function(){
 		window.close();
-	})
-  	
+	});
 	//see var at top
-  slider = document.getElementById('slider'),
-  searchfield = document.getElementById('q'),
-  srnav = document.getElementById('search_result_nav'),
-  startlink = document.getElementById('start_search'),
-  searchres = document.getElementById('search_result'),
+  slider = document.getElementById('slider');
+  searchfield = document.getElementById('q');
+  srnav = document.getElementById('search_result_nav');
+  startlink = document.getElementById('start_search');
+  searchres = document.getElementById('search_result');
   art_page_nav = document.getElementById('nav_artist_page');
-  trk_page_nav = document.getElementById('nav_tracks_page')
+  trk_page_nav = document.getElementById('nav_tracks_page');
   startlink.onclick = function(){
-  	slider.className = "screen-start";
+	slider.className = "screen-start";
   };
   srnav.onclick = function(){
-  	slider.className = "screen-search";
+	slider.className = "screen-search";
   };
 
-	artsHolder	= $('#artist-holder'),
-	artsImage	= $('img.artist-image',artsHolder),
-	artsBio		= $('p.artist-bio',artsHolder),
-	artsTracks	= $('.tracks-for-play',artsHolder),
-	artsplhld	= $('.player-holder',artsHolder),
+	artsHolder	= $('#artist-holder');
+	artsImage	= $('img.artist-image',artsHolder);
+	artsBio		= $('p.artist-bio',artsHolder);
+	artsTracks	= $('.tracks-for-play',artsHolder);
+	artsplhld	= $('.player-holder',artsHolder);
 	artsName	= $('#artist-name');
 	
-	tracksHolder = $('#tracks-holder'),
-	tracksTracks = $('.tracks-for-play', tracksHolder),
+	tracksHolder = $('#tracks-holder');
+	tracksTracks = $('.tracks-for-play', tracksHolder);
 	tracksName	 = $('#tracks-name');
-	trksplhld 	 = $('.player-holder',tracksHolder),
+	trksplhld	 = $('.player-holder',tracksHolder);
 
 
 $('.vk-auth').submit(function(){
@@ -369,27 +369,21 @@ $('.vk-auth').submit(function(){
 	var pass = $('input.vk-pass',_this).val();
 	loginxhr.send(loginxhr.xhrparams + '&email=' + encodeURIComponent(email) + '&pass=' + encodeURIComponent(pass));	
 	return false;
-})
+});
 
-
- /* document.getElementById('auth').onsubmit = function(){
-	loginxhr.xhrparams += '&email=' + encodeURIComponent($('#email')[0].value) + '&pass=' + encodeURIComponent($('#pass')[0].value);
-	loginxhr.send(loginxhr.xhrparams);	//логин
-	return false;
-  };*/
   if (widget.preferenceForKey('vkid')) {
-  	$(document.body).addClass('vk-logged-in');
-  	vk_logged_in = true;
+	$(document.body).addClass('vk-logged-in');
+	vk_logged_in = true;
   } else{
-	log('not loggin in')
-}
+	log('not loggin in');
+  }
 
 
 
 	$('#search-artist').click(function(){
 		var query = searchfield.value;
 		if (query) {
-			artistsearch(query)
+			artistsearch(query);
 		}
 		
 		
@@ -402,7 +396,7 @@ $('.vk-auth').submit(function(){
 		var query = searchfield.value;
 		if (query) {
 			trk_page_nav.innerHTML = query;
-			tracksName.text(query)
+			tracksName.text(query);
 			slider.className = 'sreen-tracks-page';
 			player_holder  = trksplhld;
 				
@@ -412,9 +406,9 @@ $('.vk-auth').submit(function(){
 				prerenderPlaylist(musicObj.playlist,tracksTracks,musicObj.links);
 			} else {
 				wait_for_vklogin = function(){
-					_this.click()
+					_this.click();
 				}
-			};
+			}
 		}
 		
 	});
