33f53074ae23696118b948d20903e7bbe97fefcd
U-51A\Gleb <Gleb@51a.(none)>
2010-01-10 20:27:52 +0300

    rf

---
+++ js/network.vk.api.js
@@ -0,0 +1,66 @@
+
+
+
+//var viewer_id 		= seesu.vk_id;
+var vk_api = function(viewer_id,s,api_id){
+	this.viewer_id 	= viewer_id;
+	this.s 			= s;
+	this.api_id 	= api_id;
+	this.api_link 	= 'http://api.vkontakte.ru/api.php';
+	this.v 			= '2.0';
+}
+
+vk_api.prototype = {
+	'use': function(method,params,callback){
+	
+		if (method) {
+			var _this = this;
+			var pv_signature_list = [], // array of <param>+<value>
+				params_full = params || {},
+				apisig =  true; // yes, we need signature
+			
+			params_full.method 	= method;
+			params_full.api_id 	= this.api_id;
+			params_full.v		= this.v;
+			params_full.format 	= params_full.format || 'json';
+			
+			if(apisig) {
+				for (var param in params_full) {
+					pv_signature_list.push(param + '=' + params_full[param]);
+					
+				}
+				
+				pv_signature_list.sort();
+				var paramsstr = '';
+				for (var i=0, l = pv_signature_list.length; i < l; i++) {
+					paramsstr += pv_signature_list[i];
+				};
+				
+				params_full.sig = hex_md5(this.viewer_id + paramsstr + this.s);
+
+			}
+			
+			$.ajax({
+			  url: _this.api_link,
+			  global: false,
+			  type: "GET",
+			  dataType: params_full.format || "XML",
+			  data: params_full,
+			  error: function(r){
+			  },
+			  success: function(r){
+				if (callback) {callback(r);}
+			  },
+			  complete: function(xhr){
+			  	log(xhr.responseText)
+			  }
+			});
+		}
+	},
+	audio_search: function(query,params,callback){
+		var params_u = params || {};
+			params_u.q = query;
+			params_u.count = params_u.count || 30;
+		this.use('audio.search',params_u,callback)
+	}
+}
---
+++ js/network.vk.login.js
@@ -0,0 +1,89 @@
+var vk_captcha;
+
+var vk_login = function(login,pass) {
+	$.ajax({
+	  url: "http://vkontakte.ru/login.php",
+	  global: false,
+	  type: "POST",
+	  dataType: "json",
+	  data: {
+		'noredirect': '1',
+		'email': login,
+		'pass': pass
+	  },
+	  error: function(){
+		log('войти не удалось');
+	  },
+	  success: function(r){
+		var vk_id,vk_error;
+		if (vk_error = r.error){
+			vk_login_error.text('Wrong login or password')
+		} else if (vk_captcha = r.captcha_sid){
+			captcha_img.attr('src','http://vkontakte.ru/captcha.php?s=1&sid=' + vk_captcha);
+			$(document.body).addClass('vk-needs-captcha');
+		} else 	if (vk_id = r.id) {
+			vk_logg_in(vk_id, r.email);
+			wait_for_vklogin && wait_for_vklogin();
+		}
+	  },
+	  complete: function(xhr){
+	  	log(xhr.responseText)
+	  }
+	});	
+}
+var vk_send_captcha = function(captcha_key,login,pass){
+	$.ajax({
+	  url: "http://vkontakte.ru/login.php",
+	  global: false,
+	  type: "POST",
+	  dataType: "json",
+	  data: {
+		'op': 'a_login_attempt',
+		'captcha_key': captcha_key,
+		'captcha_sid': vk_captcha
+   	  },
+	  success: function(r){
+	  	if (vk_captcha = r.captcha_sid){
+	  		log(vk_captcha)
+			captcha_img.attr('src','http://vkontakte.ru/captcha.php?s=1&sid=' + vk_captcha);
+			$(document.body).addClass('vk-needs-captcha');
+		}
+	  },
+	  complete: function(xhr){
+	  	log(xhr.responseText)
+		if ((xhr.responseText.indexOf('vklogin') != -1)){
+			vk_captcha = 0;
+			vk_login(login,pass);
+			
+		}
+	  }
+	});
+}
+var vk_login_check = function(){
+	$.ajax({
+	  url: "http://vkontakte.ru/feed2.php",
+	  global: false,
+	  type: "GET",
+	  dataType: "json",
+	  error: function(){
+		log('vignali!');
+		vk_logged_out();
+	  }
+	});
+};
+var vk_logg_in = function(id,email){
+	widget.setPreferenceForKey(id, 'vkid');
+	widget.setPreferenceForKey(email, 'vkemail');
+	vk_logged_in = true;
+	$(document.body).addClass('vk-logged-in');
+	log('вошли в контакте и скрыли форму логина');
+};
+var vk_logged_out = function(){
+	widget.setPreferenceForKey(false, 'vkid');
+	widget.setPreferenceForKey(false, 'vkemail');
+	log(widget.preferenceForKey('vkid'))
+	vk_logged_in = false;
+	$(document.body).removeClass('vk-logged-in');
+	log('отображаем форму логина где нужно');
+	
+};
