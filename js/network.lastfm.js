99b04f2e0ef1b1f0f9c8473d932b0dd1daf74363
U-51A\Gleb <Gleb@51a.(none)>
2010-01-13 15:07:57 +0300

    scrobling rf

--- js/network.lastfm.js
+++ js/network.lastfm.js
@@ -127,7 +127,7 @@ var lfm_scroble = {
 		last_scroble = node.data('last_scroble'),
 		timestamp = ((new Date()).getTime()/1000).toFixed(0);
 
-	if (((timestamp - starttime)/duration > 0.6) || (last_scroble && ((timestamp - last_scroble)/duration > 0.6)) ){
+	if (((timestamp - starttime)/duration > 0.2) || (last_scroble && ((timestamp - last_scroble)/duration > 0.6)) ){
 		this.music.push({
 			'artist': artist, 
 			'title': title,
