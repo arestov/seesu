c3f915a2acd834365ae7729bc78588ed8228af1d
U-GlebPC\Gleb <Gleb@GlebPC.(none)>
2010-03-26 23:31:43 +0300

    buttmen js

---
+++ js/buttmen.js
@@ -0,0 +1,98 @@
+
+var button_menu = function(jq_node){
+	this.node = jq_node;
+	this.sectors = 8;
+	var button_menu_this = this;
+	
+	var but_hl_style = $('<style></style>').appendTo(document.documentElement.firstChild)
+	
+	var style = '';
+	for (var i=1; i <= this.sectors; i++) {
+		log(i)
+		var style_ver = '\n .buttmen-hlts ' + '.buttmen-hlt-vertical-' + i + ' ';
+		var style_hor = '\n .buttmen-hlts ' + '.buttmen-hlt-horizontal-' + i + ' ';
+		style += style_hor + '.buttmen-hlt-left {opacity:' +  (1 - 1/this.sectors*i) + ';}';
+		style += style_hor + '.buttmen-hlt-right {opacity:' + (1/this.sectors*i) + ';}';
+		style += style_ver + '.buttmen-hlt-top {opacity:' +  (1 - 1/this.sectors*i) + ';}';
+		style += style_ver + '.buttmen-hlt-bottom {opacity:' + (1/this.sectors*i) + ';}';
+	};
+	but_hl_style.html(style)
+	
+	
+	jq_node.mousedown(function(e){
+		var _this = $(this);
+		var butt_hlts = _this.find('dd.buttmen-hlts ul');
+		
+		
+		_this.addClass('buttmen-highlighting');
+		
+		var butt_main = $('dt.main-button', this).mouseleave(function(e){
+			_this.removeClass('buttmen-highlighting').addClass('buttmen-butting');
+			$(this).unbind('mouseleave');
+		});
+		$(document).mouseup(function(e){
+			log('up')
+			butt_main.unbind('mouseleave');
+			setTimeout(function(){
+				_this.removeClass('buttmen-butting');
+				_this.removeClass('buttmen-highlighting')
+			},10);
+			$(document).unbind('mouseup')
+			$(document).unbind('mousemove')
+		})
+		
+		
+		var width = _this.width();
+		var height = _this.height();
+		var el_position = _this.offset();
+		var border_top 		= el_position.top;
+		var border_left 	= el_position.left;
+		var border_right 	= border_left + width;
+		var border_bottom 	= border_top + height;
+		
+		
+		var sector_vertical = height/button_menu_this.sectors;
+		var sector_horizontal = width/button_menu_this.sectors;
+		
+		$(document).mousemove(function(e){
+			var x = e.pageX;
+			var y = e.pageY;
+			if ( (x > border_left) && (x < border_right) && (y > border_top ) && (y < border_bottom)){
+				
+				var sector_y = (y - border_top)/sector_vertical;
+				
+				
+				var remainder_x = (x - border_left) % sector_horizontal;
+				var sector_x = ((x - border_left) - remainder_x)/sector_horizontal + (remainder_x ? 1 : 0);
+				
+				var remainder_y = (y - border_top) % sector_vertical;
+				var sector_y = ((y - border_top) - remainder_y)/sector_vertical + (remainder_y ? 1 : 0);
+				
+				butt_hlts.attr('class', 'buttmen-hlt-vertical-' + sector_y + ' '+ 'buttmen-hlt-horizontal-' + sector_x )
+				log(sector_x)
+				log(sector_y)
+			}
+			
+		})
+		
+	})
+	.mouseup(function(e){
+		
+		var _this = $(this);
+		_this.removeClass('buttmen-butting');
+		
+	})
+	.mousemove(function(e){
+		e.target.ownerDocument.defaultView.getSelection().removeAllRanges();
+	});
+	
+	
+	
+}
+$(function(){
+	var buttmen_node =  $('.buttmen');
+	if (buttmen_node){
+		buttmen = new button_menu(buttmen_node)
+	}
+})
+
