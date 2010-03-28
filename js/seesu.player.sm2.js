d622e742a51d743076d3bcf7741ceeadcd14b41c
U-GlebPC\Gleb <Gleb@GlebPC.(none)>
2010-03-28 12:37:45 +0400

    init sm2 commit

---
+++ js/seesu.player.sm2.js
@@ -0,0 +1,5 @@
+soundManager.url = 'http://seesu.heroku.com/swf/';
+soundManager.flashVersion = 9; // optional: shiny features (default = 8)
+soundManager.useFlashBlock = false; // optionally, enable when you're ready to dive in
+soundManager.debugMode = true;
+soundManager.wmode = 'transparent'
