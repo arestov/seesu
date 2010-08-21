02c237979c178c3c251e971a5067ad3e2c80db8d
Gleb Arestov <gr1b0k@gmail.com>
2010-08-21 12:18:39 +0400

    jsonp

--- js/_small_serv.js
+++ js/_small_serv.js
@@ -69,13 +69,12 @@ window.removeEvent = window.addEventListener ?
 		elem.detachEvent('on' + evType, fn)
 	};
 (function(){
-	var jsonp_counter = 1;
+	var jsonp_counter = 0;
 	window.create_jsonp_callback = function(func){
-		var func_name = 'jsonp_callback_' + window.jsonp_counter;
+		var func_name = 'jsonp_callback_' + (++jsonp_counter);
 		window[func_name] = func;
 		
 		
-		++window.jsonp_counter;
 		
 		return func_name;
 	}	
