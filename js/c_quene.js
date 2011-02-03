a9ef16f44f37488de0d9d296d68ebb2a40b2cc06
Gleb Arestov <gr1b0k@gmail.com>
2011-02-03 19:15:48 +0300

    quene fixes

--- js/c_quene.js
+++ js/c_quene.js
@@ -12,10 +12,56 @@ funcs_quene = function(small_delay, big_delay, big_delay_interval){
 	if (!big_delay || !big_delay_interval){
 		this.nobigdelay = true;
 	}
-	this.last_num = 0;
+	this.using_stat = [];
 };
 
 funcs_quene.prototype = {
+	get_interval: function(){
+		var last_num = this.using_stat.length - 1;
+		var bigdelay_turn = (!this.nobigdelay && last_num >1  && (((last_num + 1) % this.big_delay_interval) === 0));
+		
+		if (bigdelay_turn){
+			var real_bdinterval = (new Date()).getTime() - this.using_stat[last_num - this.big_delay_interval];
+			var _unit = (this.small_delay * this.big_delay_interval + this.big_delay);
+			console.log('diff:' + (real_bdinterval - _unit) )
+			if (real_bdinterval && real_bdinterval > _unit){
+				var time = Math.max(0, this.big_delay - (real_bdinterval - _unit));
+				console.log(real_bdinterval - _unit);
+				console.log('smaller delay: ' + time);
+			} else{
+				var time = this.big_delay;
+				console.log('big_delay delay: ' + time);
+			}
+		} else{
+			var time = this.small_delay;
+			
+			/*
+			var last_usage = this.using_stat[last_num] || 0;
+			var difference = (new Date()).getTime() - last_usage;
+			if (!last_usage){
+				var time = 0;
+			} else if (last_num % this.big_delay_interval === 0){
+				if (difference > this.big_delay){
+					if (difference - this.big_delay < this.small_delay){
+						var time = this.small_delay - (difference - this.big_delay);
+					} else{
+						var time=0;
+					}
+				} else {
+					
+				}
+				
+				
+			} else if (difference > this.small_delay){
+				var time = this.small_delay - difference;
+			} else  {
+				console.log('difference:' + difference);
+				var time = 0;
+			}*/
+		}
+			
+		return  time;
+	},
 	add: function(func, not_init){
 		var quene_just_for_me = this.big_quene;
 		var counter = quene_just_for_me.length;
@@ -27,14 +73,15 @@ funcs_quene.prototype = {
 			func: function(){
 				func();
 				this.done = true;
-				var time = (!_this.nobigdelay && (((_this.last_num + 1) % _this.big_delay_interval) === 0)) ? _this.big_delay : _this.small_delay;
+				
+				var time = _this.get_interval();
 			
 				setTimeout(function(){
 					_this.next(quene_just_for_me);
 				}, time);
-	
-				_this.last_usage = (new Date()).getTime();
-				_this.last_num++;
+				
+				_this.using_stat.push((new Date()).getTime());
+				
 	
 			
 			}
@@ -49,22 +96,15 @@ funcs_quene.prototype = {
 		return _ob;
 	},
 	init: function(){
-		if (this.big_quene.inited){return this.big_quene}
+		if (this.big_quene.inited) {return this.big_quene;}
 		var _this = this;
 
 
-		
-			var last_usage = this.last_usage || 0;
-			var difference = (new Date()).getTime() - last_usage;
-			var time =  (!_this.nobigdelay && (((_this.last_num) % _this.big_delay_interval) === 0)) ? _this.big_delay : _this.small_delay
-			if (difference < time) {
-				setTimeout(function(){
-					_this.next(_this.big_quene);
-					
-				}, time - difference)
-			} else{
-				_this.next(_this.big_quene);
-			}
+		var time = this.get_interval();
+		setTimeout(function(){
+			_this.next(_this.big_quene);
+		}, time)
+			
 		
 		this.big_quene.inited = true;
 		return this;
@@ -103,6 +143,8 @@ funcs_quene.prototype = {
 				this.reset();
 				return false;
 			}
+		} else{
+			console.log('its another q')
 		}
 	},
 	reset: function(){
