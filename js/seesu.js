86c996cef60ca8b378ac7637616edd57b76e006f
Gleb Arestov <gr1b0k@gmail.com>
2011-09-05 11:04:37 +0400

    merge

--- js/seesu.js
+++ js/seesu.js
@@ -655,7 +655,93 @@ var render_tracks_by_similar_artists = function(original_artist, no_navi, start_
 };
 
 
+var makeArrayDeeper = function(array){
+	var r = [];
+	for (var i=0; i < array.length; i++) {
+		r.push({
+			d: array[i]
+		});
+		
+	};
+	return r
+};
+var simplifyName= function(v){
+	return v && v.toLowerCase().replace(/\s/gi, '');
+};
+var markOriginalAlbum = function(target, albums, field){
+	var cached_value = simplifyName(getTargetField(target, field));
+	
+	
+	for (var i=0; i < albums.length; i++) {
+		var cur = albums[i];
+		if (cur != target){
+			var cur_value = simplifyName(getTargetField(cur, field));
+			if (cur_value.indexOf(cached_value) == 0){
+				if (!(target.acceptor && cached_value == cur_value)){
+					cur.acceptor = true;
+					delete cur.original;
+					if (!target.acceptor){
+						target.original = true;
+					}
+					
+				}
+			}
+		}
+	}
+};
 
+var checkOriginalAlbums = function(albums){
+	for (var i=0; i < albums.length; i++) {
+		markOriginalAlbum(albums[i], albums, 'd.name')
+		
+	};
+	return albums;
+};
+var sortAlbs = function(a, b){
+	return sortByRules(a, b, [
+		'acceptor',
+		{
+			field: 'original',
+			reverse: true
+		},
+		function(el){
+			return parseFloat(getTargetField(el, 'd.@attr.rank'));
+		},
+		{
+			field: 'year.text',
+			reverse: true,
+		},
+	]);
+};
+
+var sortLfmAlbums = function(r, artist){
+	var ob = {
+		own: $filter(makeArrayDeeper(toRealArray(r.topalbums.album)), 'd.artist.name', artist),
+		ordered: []
+	};
+	ob.foreign = ob.own.not;
+	checkOriginalAlbums(ob.own);
+	checkOriginalAlbums(ob.foreign);
+	
+	
+	if (ob.own.length){
+		ob.own.sort(sortAlbs);
+		var oae = $filter(ob.own, 'acceptor', true);
+		if (oae.not && oae.not.length){
+			ob.ordered.push($filter(oae.not, 'd'));
+		}
+		if (oae.length){
+			ob.ordered.push($filter(oae, 'd'));
+		}
+	}
+	if (ob.foreign.length){
+		ob.foreign.sort(sortAlbs);
+		ob.ordered.push($filter(ob.foreign, 'd'));
+		
+	}
+	
+	return ob;
+};
 
 
 
