a1461aca45b9d51d2fa8d3a7af06c1f5175a6592
Gleb Arestov <gr1b0k@gmail.com>
2011-11-10 10:20:03 +0400

    small storage rf

--- js/w_storage.js
+++ js/w_storage.js
@@ -2,9 +2,9 @@
 	var stringify = function(value){
 		return value;
 	}
-	if ((typeof JSON === 'object') && JSON.stringify){
+	if ((typeof JSON == 'object') && JSON.stringify){
 		stringify = function(value){
-			if ((typeof value === 'object') || (typeof value === 'array')){
+			if ((typeof value == 'object')){
 				return JSON.stringify(value);
 			} else{
 				return value;
@@ -21,15 +21,14 @@
 	
 	var store_get = function(){return false};
 	var store_set = function(){return false};
-	if ((typeof widget === 'object') && !widget.fake_widget && (typeof widget.setPreferenceForKey == 'function')) {
+	if ((typeof widget == 'object') && !widget.fake_widget && (typeof widget.setPreferenceForKey == 'function')) {
 		store_get = function(key){
 			return widget.preferenceForKey(key);
 		}
 		store_set = function(key, value){
 			return widget.setPreferenceForKey(stringify(value), key);
 		}
-	} else
-	if (typeof localStorage === 'object') {
+	} else if (typeof localStorage == 'object') {
 		store_get = function(key){
 			return localStorage.getItem(key);
 			
@@ -43,8 +42,7 @@
 			}
 			
 		}
-	} else
-	if ( (typeof System != "undefined") && System.Gadget && System.Gadget.Settings){
+	} else if ( (typeof System != "undefined") && System.Gadget && System.Gadget.Settings){
 		store_get = function(key){
 			return System.Gadget.Settings.readString(key);
 		}
@@ -54,16 +52,16 @@
 	}
 	var get_key = function(key){
 		var ram_value = ram_storage[key];
-		if (typeof ram_value !== 'undefined'){
+		if (typeof ram_value != 'undefined'){
 			return ram_value;
 			
 		} else{
 			return store_get(key);
 		}
 	}
-	var set_key = function(key, value, important){
+	var set_key = function(key, value, opts){
 		ram_storage[key] = value;
-		return store_set(key, value, important);
+		return store_set(key, value, opts);
 	}
 	
 	
@@ -71,12 +69,12 @@
 	
 	
 	
-	window.w_storage = function(key, value, important){
+	window.w_storage = function(key, value, opts){
 		if (key){
-			if (typeof value === 'undefined'){
+			if (typeof value == 'undefined'){
 				return get_key(key);
 			} else {
-				return set_key(key, value, important);
+				return set_key(key, value, opts);
 			}
 		} else {
 			return false
