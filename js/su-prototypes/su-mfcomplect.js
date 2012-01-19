7542b5841be72558b519e6b1789bac662727a437
Gleb Arestov <gr1b0k@gmail.com>
2012-01-19 10:48:48 +0400

    mf-complect

---
+++ js/su-prototypes/su-mfcomplect.js
@@ -0,0 +1,46 @@
+var mfComplect = function() {
+	
+};
+createPrototype(mfComplect, new servModel(), {
+	
+});
+
+
+var mfComplectsPack = function() {
+	
+};
+createPrototype(mfComplectsPack, new servModel(), {
+	
+});
+
+/*
+
+this.fire('got-results')
+
+this.fire('got-result')
+
+this.fire('error')
+
+
+this.fire('got-nothing')
+
+в процессе
+
+завершен
+
+
+
+имеет результаты
+
+0 результатов
+
+
+имеет ошибку
+
+ непоправимую ошибку
+
+ ошибку поправимую кем угодно
+ ошибку поправимую только несамостоятельно
+
+
+*/
