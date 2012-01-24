b064c48369dadf8e455257cffb8cca9de8332bd3
Gleb Arestov <gr1b0k@gmail.com>
2012-01-24 09:47:26 +0400

    origress bar for mf

--- js/prototypes/songFile.js
+++ js/prototypes/songFile.js
@@ -8,12 +8,85 @@
 
 	};
 	createPrototype(songFileModelUI, new servView(), {
+		state_change: {
+			'playing-progress': function(factor){
+				this.changeBar(this.cplayng, factor);
+			},
+			'loading-progress': function(factor){
+				this.changeBar(this.cloading, factor);
+			},
+			play: function(state){
+				if (!state){
+					this.resetPlayPosition();
+				}
+			},
+			selected: function(state) {
+				if (state){
+					this.c.addClass('selected-mf');
+				} else {
+					this.c.removeClass('selected-mf');
+				}
+			}
+		},
 		createBase: function() {
 			this.c = $('<li></li>');
-			this.title_c = $('<span></span>');
-			$('<span class="main-mf-text"></span>').text(this.sf.getTitle()).appendTo(this.title_c);
-			$('<span class="mf-source"></span>').text(this.sf.from).appendTo(this.title_c);
-			this.title_c.appendTo(this.c)
+
+			var getClickPosition = function(e, node){
+				//e.offsetX || 
+				var pos = e.pageX - $(node).offset().left;
+				return pos;
+			};
+
+			this.progress_c = $('<div class="mf-progress"></div>');
+
+
+
+			//this.c = $('<div class="track-progress"></div>')
+			var _this = this;
+			this.progress_c.click(function(e){
+				var pos = getClickPosition(e, this);
+				if (!_this.width){
+					_this.fixWidth();
+				}
+				_this.sf.setPositionByFactor(_this.width && ((pos/_this.width)));
+				//su.ui.hidePopups();
+				//e.stopPropagation();	
+			});
+			this.cloading = $('<div class="mf-load-progress"></div>').appendTo(this.progress_c);
+			this.cplayng = $('<div class="mf-play-progress"></div>').appendTo(this.progress_c);
+			this.track_text = $('<div class="mf-text"></div>').appendTo(this.progress_c);
+
+			
+
+			//this.title_c = $('<span></span>');
+			$('<span class="mf-duration"></span>').appendTo(this.track_text);
+			$('<span class="main-mf-text"></span>').text(this.sf.getTitle()).appendTo(this.track_text);
+			$('<span class="mf-source"></span>').text(this.sf.from).appendTo(this.track_text);
+			//this.title_c.appendTo(this.c);
+
+			this.c.append(this.progress_c);
+		},
+		changeBar: function(bar, factor){
+			if (factor){
+				if (this.width){
+					bar[0].style.width = factor * this.width + 'px'
+				} else {
+					bar[0].style.width = factor * 100 + '%'
+				}
+			} else {
+				bar[0].style.width = 0;
+			}
+		},
+		fixWidth: function(){
+			this.width = this.c.width();
+		},
+		resetPlayPosition: function(){
+			this.cplayng[0].style.width = 0
+		},
+		reset: function(){
+			this.resetPlayPosition();
+			this.cloading[0].style.width = 0;
+			this.fixWidth();
 		}
 	});
 
@@ -144,10 +217,10 @@
 			}
 		},
 		activate: function() {
-			
+			this.updateState('selected', true);
 		},
 		deactivate: function() {
-			
+			this.updateState('selected', false);
 		},
 		markAsPlaying: function() {
 			
