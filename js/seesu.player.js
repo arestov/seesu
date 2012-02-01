94226d349f48c3cc5225bad49abfa8c92ac66d15
Gleb Arestov <gr1b0k@gmail.com>
2012-02-01 19:04:30 +0400

    rf

--- js/seesu.player.js
+++ js/seesu.player.js
@@ -60,171 +60,6 @@ su.player = {
 
 
 
-
-var change_volume = function (volume_value){
-  suStore('vkplayer-volume', volume_value, true);
-  su.player.player_volume = volume_value;	
-}
-
-var sm2iframed = {
-	c: $('<iframe id="i_f_sm2" src="http://seesu.me/i.html" ></iframe>'),
-	text_of_function: function(func){
-		return func.toString().replace(/^.*\n/, "").replace(/\n.*$/, "")
-	},
-	init: function(){
-		if (!su.env.cross_domain_allowed){
-			return false;
-		}
-		var _this = this;
-	
-		if (this.c) {
-			
-			var last_iframe_func = this.text_of_function(_this.i_func).replace('_volume', su.player.player_volume );
-			var scripts_paths = [
-				bpath + 'js/common-libs/soundmanager2.js',
-				bpath + 'js/seesu.player.sm2.js'
-			];
-
-			scripts_data = [];
-			
-			var all_scripts_data_loaded = false;
-			var wait_for_all_script_data = false;
-			var add_script_data_callback = function(){return;};
-			var send_scripts_to_iframe = function(iframe){
-				if (all_scripts_data_loaded){
-					console.log('sending')
-					iframe.contentWindow.postMessage("append_data_as_script\n" + scripts_data.complete_data, '*');
-					
-				} else{
-					console.log('callbacking')
-					wait_for_all_script_data = true;
-					add_script_data_callback = function(){
-						send_scripts_to_iframe(iframe);
-					}
-				}
-			};
-			
-			var sort_by_number_order = function(g,f){
-				if (g && f) {
-					if (g.number > f.number)
-						{return 1;}
-					else if (g.number < f.number)
-						{return -1;}
-					else
-					{return 0;}
-				} else {return 0;}
-	
-			};
-			
-			var add_script_data = function(i, l, data){
-				scripts_data.push({"number": i, "data": data});
-				if (scripts_data.length == (l)){
-					scripts_data.sort(sort_by_number_order);
-					scripts_data.complete_data = '/*<![CDATA[*/' + '\n';
-					for (var m=0; m < scripts_data.length; m++) {
-						scripts_data.complete_data += scripts_data[m].data + '\n\n'
-					};
-					scripts_data.complete_data += 'var sm2_init_p = ' + JSON.stringify(_this.i_params) + ';';
-					scripts_data.complete_data += last_iframe_func;
-					scripts_data.complete_data += '/* ]]>*/';
-	
-					all_scripts_data_loaded = true;
-					if (wait_for_all_script_data) {
-						add_script_data_callback();
-					}
-				}
-			};
-			
-			if (scripts_paths.length) {
-				var get_js = function(i,l){
-					$.ajax({
-						url: scripts_paths[i].replace(location.href, ''),
-						global: false,
-						dataType: 'text',
-						type: "GET",
-						complete: function(xhr){
-							add_script_data(i, l, xhr.responseText);
-						}
-					});
-				}
-				for (var i=0; i < scripts_paths.length; i++) {
-					get_js(i, scripts_paths.length);
-				}
-			}
-			
-			var check_iframe = function(e){
-				if (e.data.match(/iframe_loaded/)){
-					
-					console.log('got iframe loaded feedback');
-					send_scripts_to_iframe(_this.c[0]);
-					
-					
-				} else if (e.data.match(/sm2_inited/)){
-					console.log('iframe sm2 wrokss yearh!!!!');
-					yepnope({
-						load:  [bpath + 'js/seesu.player.sm2.js'],
-						complete: function(){
-							su.player.musicbox = new sm2_p(su.player.player_volume, false, _this.c);
-						}
-					});
-					
-					clearTimeout(html_player_timer);
-					_this.c.addClass('sm-inited');
-					dstates.add_state('body','flash-internet');
-					$('#sm2-container').remove();
-					removeEvent(window, "message", check_iframe);
-				}
-			};
-			addEvent(window, "message", check_iframe);
-			
-			
-			_this.c.on('load',function(){
-				console.log('source knows that iframe loaded');
-				this.contentWindow.postMessage("test_iframe_loading_state", '*');
-				
-			});
-			
-			suReady(function(){
-				//$(document.body).append(_this.c);
-			});
-			
-	
-		}
-	},
-	remove: function(){
-		this.c.remove();
-	},
-	i_params: {
-		oext: su.env.opera_extension
-	},
-	i_func: function(){
-					
-		window.soundManager = new SoundManager('http://seesu.me/swf/', false, {
-			flashVersion : 9,
-			useFlashBlock : true,
-			debugMode : false,
-			wmode : sm2_init_p.oext ? 'opaque' : 'transparent',
-			useHighPerformance : !sm2_init_p.oext
-		});
-		if (soundManager){			
-			sm2_p_in_iframe = new sm2_p(_volume, soundManager);
-			sm2_p_in_iframe.player_source_window = iframe_source;
-			soundManager.onready(function() {
-				if (soundManager.supported()) {
-					iframe_source.postMessage("sm2_inited",'*');
-				} else{
-					console.log('by some reason sm2 iframe don"t work')
-				}
-			});
-		} else{
-			console.log('no sounds');
-		}
-
-		
-		
-	}
-};
-
 var html_player_timer;
 (function(){
 	return
@@ -276,20 +111,16 @@ var html_player_timer;
 							suReady(function(){
 								dstates.add_state('body','flash-internet');
 							})
-							sm2iframed.remove();
 							clearTimeout(html_player_timer);
 						} else {
-							console.log('sm2 in widget notok')
-							sm2iframed.init();
+							console.log('sm2 in widget notok');
 					
 						}
 					});
 				}
 			}
 		})
-	} else {
-		sm2iframed.init();
-	}
+	} 
 })();
 
 
