fd79cce64b72f57149046a8832cd419a961fc0b9
Gleb Arestov <gr1b0k@gmail.com>
2013-05-01 13:22:08 +0400

    rf

--- loader.js
+++ loader.js
@@ -1,3 +1,7 @@
+var big_timer;
+(function(){
+"use strict";
+
 requirejs.config({
 	paths: {
 		provoda: 'js/libs/provoda',
@@ -53,10 +57,8 @@ requirejs.config({
 
 })();
 
-
-var bpath = '';
 window._gaq = window._gaq || [];
-var big_timer = {
+big_timer = {
 	setN: function(name){
 		var time = new Date() * 1;
 		if (name){
@@ -72,49 +74,5 @@ var big_timer = {
 	"page-start": new Date() * 1,
 	q: []
 };
-		
-(function(){
-function isFileReady ( readyState ) {
-    // Check to see if any of the ways a file can be ready are available as properties on the file's element
-    return ( ! readyState || readyState == 'loaded' || readyState == 'complete' );
-}
-	
-	
-var p = document.getElementsByTagName('script');
-p = p[p.length-1];
-		
-window.loadJS = function(src, callback){
-	var s = document.createElement('script'),
-		done;
-	s.onreadystatechange = s.onload = function () {
 
-	if ( ! done && isFileReady( s.readyState ) ) {
-
-		// Set done to prevent this function from being called twice.
-		done = true;
-		callback();
-
-		// Handle memory leak in IE
-		s.onload = s.onreadystatechange = null;
-	}
-	};
-	s.src = src;
-	p.parentNode.insertBefore(s, p);
-};
 })();
-
-
-
-loadJS(bpath + 'js/common-libs/yepnope.1.5.4-min.js', function(){
-
-
-	yepnope({
-		load: bpath + 'js/_seesu.jslist.js',
-		complete: function(){
-			require(['app_serv'], function(app_serv) {
-			});
-			
-			
-		}
-	});
-});
