e5df20e6eef0729b6cf1aa66cd178aef86a21b86
Gleb Arestov <gr1b0k@gmail.com>
2014-04-29 11:24:27 +0400

    hover changes

--- js/modules/fixCSSPixels.js
+++ js/modules/fixCSSPixels.js
@@ -27,36 +27,25 @@ define(['app_serv', 'jquery', 'spv'], function(app_serv, $, spv) {
 			if (cur.style && cur.style.cssText.indexOf('px') != -1){
 				var rulll = app_serv.culculateRemRule(cur, root_font_size);
 
-				var sel_prev_text = sheet_string.slice(0, rulll.rule_start);
+				var sel_prev_text = sheet_string.slice(0, cur.__starts);
 
 				var sel_tabs = sel_prev_text.match(/\t+(?:$)/gi);
 				var sel_tabs_count = sel_tabs && sel_tabs[0].length || 0;
-				//sel_tabs_count += 1;
-				
-			//	console.log(sel_tabs_count);
+
 				complects.push(rulll);
 				if (rulll.px_props.length){
-					rulll.full_string = '\n' +
-						app_serv.getTabs(sel_tabs_count) + rulll.stretch_selector + '{\n' +
-						app_serv.getTabs(sel_tabs_count + 1) + '/* rem hack */\n' +
-						app_serv.getRulesString(rulll.px_props, sel_tabs_count + 1) +
-						app_serv.getTabs(sel_tabs_count + 1) + '}\n';
 
 
 					var rules_string = '\n' +
 						app_serv.getTabs(sel_tabs_count + 1) + '/* rem hack */\n' +
 						app_serv.getRulesString(rulll.px_props, sel_tabs_count + 1);
 
-					var big_start = big_result.slice(0, rulll.rule_end -1 + pos_shift);
-					var big_end = big_result.slice(rulll.rule_end -1 + pos_shift);
+					var big_start = big_result.slice(0, cur.__ends -1 + pos_shift);
+					var big_end = big_result.slice(cur.__ends -1 + pos_shift);
 
 					big_result = big_start + rules_string + big_end;
 					pos_shift += rules_string.length;
 
-				//	big_string += rulll.full_string;
-
-					//getRulesString
-				//	big_string += rulll.stretch_selector + ' {\n' + '\t/* rem hack */\n' + rulll.string + '}\n\n';
 				}
 				
 			}
@@ -64,6 +53,47 @@ define(['app_serv', 'jquery', 'spv'], function(app_serv, $, spv) {
 		}
 
 		return string ? big_result : complects;
+	};
+
+	var fixHovers = function(href, sheet_string, root_font_size, string) {
+		var sheet = window.CSSOM.parse(sheet_string);
+		href = href || sheet.href;
+		var pos_shift = 0;
+
+		var big_result = sheet_string;
+		var simple_rules = app_serv.getSimpleCSSRules(sheet);
+		var complects = [];
+
+
+		for (var i = 0; i < simple_rules.length; i++) {
+			var cur = simple_rules[i];
+			if (cur.selectorText && cur.selectorText.indexOf(':hover') != -1) {
+
+				var sel_prev_text = sheet_string.slice(0, cur.__starts);
+
+				var sel_tabs = sel_prev_text.match(/\t+(?:$)/gi);
+				var sel_tabs_count = sel_tabs && sel_tabs[0].length || 0;
+
+
+				//var sel_parts = cur.selectorText.split(/\s*\,\s*/);
+
+				var rules_string = '\n' +
+					app_serv.getTabs(sel_tabs_count + 1) + '/* bg hover hack */\n' +
+					app_serv.getTabs(sel_tabs_count + 1) + 'background-color:' + '#a82c53;' + '\n';
+
+				var big_start = big_result.slice(0, cur.__ends -1 + pos_shift);
+				var big_end = big_result.slice(cur.__ends -1 + pos_shift);
+
+				big_result = big_start + rules_string + big_end;
+				pos_shift += rules_string.length;
+				//
+				//debugger;
+			}
+			
+		}
+
+		return string ? big_result : complects;
+
 	};
 	var checkPX = function(url, root_font_size) {
 		//var complects = [];
@@ -81,13 +111,17 @@ define(['app_serv', 'jquery', 'spv'], function(app_serv, $, spv) {
 			url: url
 		})
 		.done(function(r) {
-			var test = createStyleSheet(url, r, root_font_size, true);
+			var fixed_css;
+
+			fixed_css = createStyleSheet(url, r, root_font_size, true);
 			//console.log("url: " + url);
 			//console.log(test);
 
+			fixed_css = fixHovers(url, fixed_css, root_font_size, true);
+
 			var file_name = url.split('/');
 			file_name = file_name && file_name[file_name.length -1];
-			window.open('data:text/plain;base64,' + btoa( '/*\n' + file_name + "\n*/ \n" + test));
+			window.open('data:text/plain;base64,' + btoa( '/*\n' + file_name + "\n*/ \n" + fixed_css));
 		});
 		
 		return big_string;
