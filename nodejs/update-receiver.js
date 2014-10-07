2016f5c35038464224228784cabf655f30361274
Gleb Arestov <gr1b0k@gmail.com>
2014-10-07 09:21:15 +0400

    updater js

---
+++ nodejs/update-receiver.js
@@ -0,0 +1,45 @@
+(function() {
+	"use strict";
+	var request = require('./updater_libs/node_modules/request'),
+	fs = require('fs'),
+	rm = require('./updater_libs/node_modules/rimraf'),
+	path = require('path'),
+	Zip = require('./updater_libs/node_modules/adm-zip');
+
+
+	//var appPath = path.dirname(process.execPath);
+	var appPath = process.cwd();
+
+	var handleZip = function( tempZipFilePath, usedPackageKey ) {
+		var installDir = path.join( appPath, usedPackageKey );
+		var zip = new Zip( tempZipFilePath );
+		zip.extractAllTo(installDir, true);
+		require( installDir );
+	};
+
+	var packageKey = 'install-pack';
+	var getUpdatePackage = function(packageUrl, cursomPackageKey) {
+		//console.log('requesting ' + packageUrl);
+
+		var usedPackageKey = cursomPackageKey || packageKey;
+
+		var tempZipFilePath = path.join( appPath, usedPackageKey + '.zip' );
+
+		var requestStream = request( packageUrl );
+
+		var writeStream = fs.createWriteStream( tempZipFilePath );
+		requestStream.pipe( writeStream );
+
+
+		writeStream.on('finish', function() {
+			//console.log('writed');
+			handleZip( tempZipFilePath, usedPackageKey );
+		});
+
+	};
+
+	
+
+	module.exports = getUpdatePackage;
+	
+})();
