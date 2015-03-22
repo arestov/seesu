68185cdc1efb876419dbdfb86741d066cec7560d
Gleb Arestov <gr1b0k@gmail.com>
2015-03-23 00:01:56 +0300

    stch fix

--- js/libs/TorrentsAudioSearch.js
+++ js/libs/TorrentsAudioSearch.js
@@ -168,7 +168,7 @@ pv.Model.extendTo(Torrent, {
 	],
 	'stch-invalid': function(target, state) {
 		if (state) {
-			disallowed_links[this.state('url')] = true;
+			disallowed_links[target.state('url')] = true;
 		}
 	},
 	'state-files_list_raw': [
@@ -215,23 +215,22 @@ pv.Model.extendTo(Torrent, {
 		}
 	],
 	'stch-torrent_required': function(target, state) {
-		var _this = this;
 		if (state ) {
-			clearTimeout(this.remove_timeout);
-			this.remove_timeout = null;
-			if (!this.troxent && !this.queued) {
-				this.queued = this.queue.add(function() {
-					//torrents_manager.get(_this.state('url'));
+			clearTimeout(target.remove_timeout);
+			target.remove_timeout = null;
+			if (!target.troxent && !target.queued) {
+				target.queued = target.queue.add(function() {
+					//torrents_manager.get(target.state('url'));
 					//_this.troxent = true;
-					_this.troxent = torrents_manager.get(_this.state('url'));
-					_this.useInterface('troxent', _this.troxent);
-					_this.queued = null;
+					target.troxent = torrents_manager.get(target.state('url'));
+					target.useInterface('troxent', target.troxent);
+					target.queued = null;
 				});				
 			}
 		} else {
-			if (this.troxent && !this.remove_timeout) {
-				this.remove_timeout = setTimeout(function() {
-					_this.destroyPeerflix();
+			if (target.troxent && !target.remove_timeout) {
+				target.remove_timeout = setTimeout(function() {
+					target.destroyPeerflix();
 				}, 7000);
 			}
 		}
