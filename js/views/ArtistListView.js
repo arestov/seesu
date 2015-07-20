d58b65faf2bf9d8aac0e53b575c864eb2a784c30
Gleb Arestov <gr1b0k@gmail.com>
2015-07-20 08:56:27 +0300

    rf

--- js/views/ArtistListView.js
+++ js/views/ArtistListView.js
@@ -1,52 +1,11 @@
-define(['pv', 'jquery'], function(pv, $) {
+define(['pv', 'jquery'], function(pv) {
 "use strict";
-
-
-var ArtcardViewInList = function() {};
-pv.View.extendTo(ArtcardViewInList, {
-	createBase: function() {
-		this.c = $('<li class="artist_in_list"></li>');
-		this.alink = $('<span class=""></span>').appendTo(this.c);
-		var _this = this;
-		this.c.click(function() {
-			_this.RPCLegacy('showArtcard');
-			//_this.RPCLegacy('showArtcard');
-			return false;
-		});
-		this.image_place = $('<span class="song-image-con"></span>').appendTo(this.c);
-		this.addWayPoint(this.c);
-	},
-	'stch-artist_name': function(target, state) {
-		target.alink.text(state);
-	},
-	'stch-selected_image': function(target, lfm_wrap) {
-		if (!lfm_wrap){
-			return;
-		}
-		var url = lfm_wrap.lfm_id ? 'http://userserve-ak.last.fm/serve/64s/' + lfm_wrap.lfm_id : lfm_wrap.url;
-
-
-		if (url){
-			target.image_place.empty();
-			target.image_place.append(
-				$('<img/>').attr('src', url)
-			);
-		}
-	}
-});
-
 
 var ArtistListView = function() {};
 pv.View.extendTo(ArtistListView, {
 	base_tree: {
 		sample_name: 'artists_list'
-	},
-	children_views: {
-		artists_list: {
-			main: ArtcardViewInList
-		}
-	},
-	'collch-artists_list': 'tpl.ancs.listc'
+	}
 });
 
 return ArtistListView;
