aff26f584f2ed92e9a0d2483694afc9a43902c2c
Gleb Arestov <gr1b0k@gmail.com>
2015-12-20 13:38:53 +0300

    Fix init style

--- js/models/YoutubeVideo.js
+++ js/models/YoutubeVideo.js
@@ -1,27 +1,23 @@
-define(['js/libs/BrowseMap', 'app_serv', 'pv'],function(BrowseMap, app_serv, pv) {
+define(['js/libs/BrowseMap', 'app_serv', 'pv', 'spv'],function(BrowseMap, app_serv, pv, spv) {
 "use strict";
 var pvUpdate = pv.update;
 var app_env = app_serv.app_env;
-var YoutubeVideo = function() {};
-BrowseMap.Model.extendTo(YoutubeVideo, {
+var YoutubeVideo = spv.inh(BrowseMap.Model, {
+	init: function(target, opts, params) {
+		target.mo = target.map_parent.mo;
+		target.map_parent = target.map_parent.map_parent;//hack, fixme
+
+
+		pvUpdate(target, 'yt_id', params.yt_id);
+
+		pvUpdate(target, 'cant_show', params.cant_show);
+		pvUpdate(target, 'previews', params.previews);
+
+		pvUpdate(target, 'nav_title', params.title);
+		pvUpdate(target, 'url_part', '/youtube/' + params.yt_id);
+	}
+}, {
 	model_name: 'youtube_video',
-	init: function(opts, params) {
-		//this.map_parent = opts.map_parent.map_parent;
-
-		//opts.map_parent = opts.map_parent
-		this._super.apply(this, arguments);
-		this.mo = this.map_parent.mo;
-		this.map_parent = this.map_parent.map_parent;//hack, fixme
-
-
-		pvUpdate(this, 'yt_id', params.yt_id);
-
-		pvUpdate(this, 'cant_show', params.cant_show);
-		pvUpdate(this, 'previews', params.previews);
-
-		pvUpdate(this, 'nav_title', params.title);
-		pvUpdate(this, 'url_part', '/youtube/' + params.yt_id);
-	},
 	'stch-mp_has_focus': function(target, state) {
 		if (state) {
 			target.app.trackEvent('Navigation', 'youtube video');
