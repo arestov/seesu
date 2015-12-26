e2383122364b893dfc4cd8cdc819952058f12bfb
Gleb Arestov <gr1b0k@gmail.com>
2015-12-26 18:36:32 +0300

    Move code

--- js/models/AppModelBase.js
+++ js/models/AppModelBase.js
@@ -1,6 +1,10 @@
 define(['pv', 'spv', '../libs/BrowseMap'], function(pv, spv, BrowseMap) {
 "use strict";
 var binded_models = {};
+
+var getStruc = BrowseMap.getStruc;
+var getStrucSources = BrowseMap.getStrucSources;
+
 var AppModelBase = spv.inh(pv.Model, {
 	init: function(target) {
 		target.navigation = [];
@@ -278,59 +282,8 @@ var AppModelBase = spv.inh(pv.Model, {
 
 	},
 	pushVDS: (function() {
-		var path = 'm_children.children.map_slice.main.m_children.children_by_mn.pioneer';
-		var children_path = 'm_children.children_by_mn.pioneer';
-
 		var getSources = spv.memorize(function(md, used_data_structure, app){
-			var struc;
-
-			var model_name = md.model_name;
-
-			var dclrs_fpckgs = used_data_structure.collch_dclrs;
-			var dclrs_selectors = used_data_structure.collch_selectors;
-
-			var bwlev_dclr = pv.$v.selecPoineertDeclr(dclrs_fpckgs, dclrs_selectors, 'map_slice', model_name, 'main', true);
-			if (bwlev_dclr) {
-				var path_mod = 'm_children.children.map_slice.' + (bwlev_dclr.space || 'main');
-
-				//+ '.m_children.children_by_mn.pioneer';
-				var bwlev_struc = spv.getTargetField(used_data_structure, path_mod);
-				var bwlev_dclrs_fpckgs = bwlev_struc.collch_dclrs;
-				var bwlev_dclrs_selectors = bwlev_struc.collch_selectors;
-
-				var pioneer_model_name = bwlev_dclr.is_wrapper_parent ? md.map_parent.model_name : model_name;
-				var md_dclr = pv.$v.selecPoineertDeclr(bwlev_dclrs_fpckgs, bwlev_dclrs_selectors, 'pioneer', pioneer_model_name, (bwlev_dclr.space || 'main'), true);
-
-				var children = spv.getTargetField(bwlev_struc, children_path);
-
-				struc = spv.getTargetField(children, [pioneer_model_name, md_dclr.space]) || spv.getTargetField(children, ['$default', md_dclr.space]);
-
-				if (bwlev_dclr.is_wrapper_parent) {
-					var nestings = struc.m_children.children;
-					var Constr = md.constructor;
-					for (var nesting_name in nestings) {
-						var items = BrowseMap.getNestingConstr(app, md.map_parent, nesting_name);
-						if (items) {
-							if (Array.isArray(items)) {
-								if (items.indexOf(Constr) != -1) {
-									struc = nestings[nesting_name];
-									break;
-								}
-							} else {
-								if (items == Constr) {
-									struc = nestings[nesting_name];
-									break;
-								}
-							}
-						}
-					}
-				}
-			} else {
-				var default_struc = spv.getTargetField(used_data_structure, path)[ '$default' ];
-				struc = spv.getTargetField(used_data_structure, path)[ model_name ] || default_struc;
-			}
-
-			return BrowseMap.getStrucSources(md, struc);
+			return getStrucSources(md, getStruc(md, used_data_structure, app));
 		}, function(md){
 			return md.constr_id;
 		});
@@ -341,7 +294,6 @@ var AppModelBase = spv.inh(pv.Model, {
 			}
 			var sources = getSources(md, this.used_data_structure);
 			pv.update(md, 'map_slice_view_sources', [md._network_source, sources]);
-
 		};
 	})(),
 	knowViewingDataStructure: function(constr_id, used_data_structure) {
