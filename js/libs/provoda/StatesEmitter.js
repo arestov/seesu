0fb0402423c4da2d8c3bbf9521734c6789cb4cd9
Gleb Arestov <gr1b0k@gmail.com>
2016-05-13 10:08:13 +0300

    Fix code style

--- js/libs/provoda/StatesEmitter.js
+++ js/libs/provoda/StatesEmitter.js
@@ -416,19 +416,21 @@ add({
 			};
 			for (prop in self){
 
-				if (getUnprefixed(prop)){
-					var cur = self[prop];
-					if (cur.event_name){
-						if (!self.reg_fires.by_namespace){
-							self.reg_fires.by_namespace = {};
-						}
-						self.reg_fires.by_namespace[cur.event_name] = cur;
-					} else if (cur.test){
-						if (!self.reg_fires.by_test){
-							self.reg_fires.by_test = [];
-						}
-						self.reg_fires.by_test.push(cur);
+				if (!getUnprefixed(prop)){
+					continue;
+				}
+
+				var cur = self[prop];
+				if (cur.event_name){
+					if (!self.reg_fires.by_namespace){
+						self.reg_fires.by_namespace = {};
 					}
+					self.reg_fires.by_namespace[cur.event_name] = cur;
+				} else if (cur.test){
+					if (!self.reg_fires.by_test){
+						self.reg_fires.by_test = [];
+					}
+					self.reg_fires.by_test.push(cur);
 				}
 			}
 		};
