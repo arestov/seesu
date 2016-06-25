f2f9386640f4d53cb3ae03eb1b5a34f6df3c8380
Gleb Arestov <gr1b0k@gmail.com>
2016-06-25 23:58:31 +0300

    Use pv-effects in lfm auth

--- js/LfmAuth.js
+++ js/LfmAuth.js
@@ -13,13 +13,6 @@ var LfmLogin = spv.inh(pv.Model, {
 			pvUpdate(target, 'deep_sandbox', true);
 		}
 
-		if (target.auth && target.auth.data_wait){
-			target.waitData();
-		} else {
-			target.auth.on('data_wait', function(){
-				target.waitData();
-			});
-		}
 		return target;
 	}
 }, {
@@ -27,15 +20,7 @@ var LfmLogin = spv.inh(pv.Model, {
 	'compx-has_session': [[
 		'@one:session:auth'
 	]],
-	triggerSession: function() {
-		pvUpdate(this, 'has_session', true);
-	},
-	waitData: function() {
-		pvUpdate(this, 'data_wait', true);
-	},
-	notWaitData: function() {
-		pvUpdate(this, 'data_wait', false);
-	},
+	'compx-data_wait':[['@one:data_wait:auth']],
 	'compx-default_access_desc': [['#locales.to-get-access']],
 	'compx-request_description': [
 		['access_desc', 'default_access_desc', '#locales.lfm-auth-invitation'],
@@ -92,14 +77,15 @@ var LfmAuth = spv.inh(pv.Model, {
 		target.opts = params || {};
 		target.has_session = !!target.api.sk;
 		target.deep_sanbdox = !!params.deep_sanbdox;
+
 		pvUpdate(target, 'session', !!target.has_session);
+		pvUpdate(target, 'bridge_url', params.bridge_url);
+
 	},
 }, {
 
-	requestAuth: function(p){
-
-		this.authInit(p || {});
-		return;
+	requestAuth: function() {
+		pvUpdate(this, 'requested', Date.now());
 	},
 	login: function(r, callback){
 		this.api.sk = r.session.key;
@@ -120,52 +106,92 @@ var LfmAuth = spv.inh(pv.Model, {
 		o.link += '&cb=' + encodeURIComponent(link_tag);
 		return o;
 	},
-	waitData: function() {
-		this.trigger('data_wait');
-		this.data_wait = true;
-	},
-	createAuthFrame: function(first_key){
-		if (this.lfm_auth_inited){
-			return false;
+	'compx-data_wait': [
+		['requested_once', 'session'],
+		function (once, session) {
+			return once && !session;
 		}
-		var _this = this;
-		var i = this.auth_frame = window.document.createElement('iframe');
-		spv.addEvent(window, 'message', function(e){
-			var iframe_win = _this.auth_frame && _this.auth_frame.contentWindow;
-			if (e.source != iframe_win) {
-				return;
-			}
-			if (e.data == 'lastfm_bridge_ready:'){
-				e.source.postMessage("add_keys:" + first_key, '*');
-			} else if(e.data.indexOf('lastfm_token:') === 0){
-				_this.setToken(e.data.replace('lastfm_token:',''));
-				console.log('got token!!!!');
-				console.log(e.data.replace('lastfm_token:',''));
-			}
-		});
-		i.className = 'serv-container';
-		i.src = this.opts.bridge_url;
-		window.document.body.appendChild(i);
-		this.lfm_auth_inited = true;
-	},
-	setAuthBridgeKey: function(key){
-		if (!this.lfm_auth_inited){
-			this.createAuthFrame(key);
-		} else{
-			this.auth_frame.contentWindow.postMessage("add_keys:" + key, '*');
+	],
+	'compx-requested_once': [
+		['requested_once', 'requested'],
+		function (once, req) {
+			return Boolean(once || req);
 		}
-	},
-	authInit: function(p){
-		var init_auth_data = this.getInitAuthData();
-
-		if (init_auth_data.bridgekey){
-			this.setAuthBridgeKey(init_auth_data.bridgekey);
+	],
+	'stch-requested': function (target, state) {
+		if (!state) {
+			return;
 		}
 
-		if (!p.not_open){
-			this.trigger('want-open-url', init_auth_data.link, init_auth_data);
-			this.waitData();
+		target.updateState('auth_data', target.getInitAuthData());
+	},
+	'compx-bridge_key': [
+		['auth_data.bridgekey']
+	],
+	'api-window': function () {
+		return window;
+	},
+	'api-bridge': [
+		'requested_once',
+		function () {
+			return window.document.createElement('iframe');
 		}
+	],
+	'state-token': [
+		['window', 'bridge'],
+		function (update, win, bridge) {
+			return spv.listenEvent(win, 'message', function(e) {
+				if (e.source != bridge.contentWindow || !spv.startsWith(e.data, 'lastfm_token:')) {
+					return;
+				}
+
+				update(e.data.replace('lastfm_token:', ''));
+			});
+		}
+	],
+	'state-_bridge_ready': [
+		['window', 'bridge'],
+		function (update, win, bridge) {
+			return spv.listenEvent(win, 'message', function(e) {
+				if (e.source != bridge.contentWindow || e.data != 'lastfm_bridge_ready:') {
+					return;
+				}
+
+				update(true);
+			});
+		}
+	],
+	'effect-started_bridge': [
+		['bridge', 'window'],
+		'bridge_url',
+		[['bridge_url']],
+		function (bridge, win, bridge_url) {
+			bridge.className = 'serv-container';
+			bridge.src = bridge_url;
+			win.document.body.appendChild(bridge);
+		}
+	],
+	'effect-prepared_bridge': [
+		'bridge',
+		[['_bridge_ready', 'bridge_key']], // возможен только при выполнении условия
+		[
+			['bridge_key'], // инвалидирующие состояния
+			['started_bridge'] // задаёт порядок выполнения (нужно чтобы выполнилось хоть раз)
+		],
+		function (bridge, key) {
+			bridge.contentWindow.postMessage("add_keys:" + key, '*');
+		}
+	],
+	'effect-asked_permission': [
+		['self'],
+		[['auth_data']],
+		[['auth_data'], ['prepared_bridge']],
+		function (self, data) {
+			self.trigger('want-open-url', data.link, data.opts);
+		}
+	],
+	'stch-token': function (target, token) {
+		target.setToken(token);
 	},
 	setToken: function(token){
 		this.newtoken = token;
