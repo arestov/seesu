dedd523791d19c337d9b96f9d79e94288f809898
Gleb Arestov <gr1b0k@gmail.com>
2016-08-07 14:20:15 +0300

    Move code of `getPatchedTree`

--- js/libs/provoda/pvTemplate/PvSimpleSampler.js
+++ js/libs/provoda/pvTemplate/PvSimpleSampler.js
@@ -4,21 +4,12 @@ var $ = require('jquery');
 var d_parsers = require('./directives_parsers');
 var config = d_parsers.config;
 var getCachedPVData = require('./getCachedPVData');
-var patchNode = require('./patchNode');
+
+var getPatchedTree = require('./getPatchedTree');
 var spv = require('spv');
 var parseEasy = require('./parseEasy');
 var parse = require('./parse');
 var getNodeInstanceCount = getCachedPVData.getNodeInstanceCount;
-
-
-function getCommentPVData(cur_node, struc_store, getSample) {
-	return getCachedPVData(cur_node, struc_store, true, getSample);
-}
-
-function getPVData(cur_node, struc_store, getSample) {
-	return getCachedPVData(cur_node, struc_store, false, getSample);
-}
-
 
 var PvSimpleSampler = (function(){
 	var push = Array.prototype.push;
@@ -181,45 +172,6 @@ var PvSimpleSampler = (function(){
 		};
 
 	})();
-
-
-	var getPatchedTree = function(node, struc_store, getSample, opts) {
-		// var result = [];
-
-		var match_stack = [ node ], i = 0;
-		while (match_stack.length){
-			var cur_node = match_stack.shift();
-			var is_start_node = node === cur_node;
-			var node_type = cur_node.nodeType;
-			var directives_data = null;
-			if (node_type == 1){
-				directives_data = getPVData(cur_node, struc_store, getSample);
-				// result.push(cur_node, directives_data);
-			} else if (node_type == 8) {
-				directives_data = getCommentPVData(cur_node, struc_store, getSample);
-				// result.push(cur_node, directives_data);
-			}
-
-			var patched = !is_start_node && patchNode(cur_node, struc_store, directives_data, getSample, opts);
-			if (patched) {
-				match_stack.unshift(patched);
-			}
-
-
-			// if (directives_data.replacer) {
-			// 	match_stack.unshift(directives_data.node);
-			// }
-
-			if (node_type == 1){
-				for (i = 0; i < cur_node.childNodes.length; i++) {
-					match_stack.push(cur_node.childNodes[i]);
-				}
-			}
-
-		}
-		// return result;
-		return node;
-	};
 
 	var buildClone = function(onode, struc_store, sample_id) {
 		var cloned = onode.cloneNode(true);
---
+++ js/libs/provoda/pvTemplate/getPatchedTree.js
@@ -0,0 +1,52 @@
+define(function(require) {
+'use strict';
+var getCachedPVData = require('./getCachedPVData');
+var patchNode = require('./patchNode');
+
+function getCommentPVData(cur_node, struc_store, getSample) {
+	return getCachedPVData(cur_node, struc_store, true, getSample);
+}
+
+function getPVData(cur_node, struc_store, getSample) {
+	return getCachedPVData(cur_node, struc_store, false, getSample);
+}
+
+return function getPatchedTree(node, struc_store, getSample, opts) {
+  // var result = [];
+
+  var match_stack = [ node ], i = 0;
+  while (match_stack.length){
+    var cur_node = match_stack.shift();
+    var is_start_node = node === cur_node;
+    var node_type = cur_node.nodeType;
+    var directives_data = null;
+    if (node_type == 1){
+      directives_data = getPVData(cur_node, struc_store, getSample);
+      // result.push(cur_node, directives_data);
+    } else if (node_type == 8) {
+      directives_data = getCommentPVData(cur_node, struc_store, getSample);
+      // result.push(cur_node, directives_data);
+    }
+
+    var patched = !is_start_node && patchNode(cur_node, struc_store, directives_data, getSample, opts);
+    if (patched) {
+      match_stack.unshift(patched);
+    }
+
+
+    // if (directives_data.replacer) {
+    // 	match_stack.unshift(directives_data.node);
+    // }
+
+    if (node_type == 1){
+      for (i = 0; i < cur_node.childNodes.length; i++) {
+        match_stack.push(cur_node.childNodes[i]);
+      }
+    }
+
+  }
+  // return result;
+  return node;
+};
+
+});
