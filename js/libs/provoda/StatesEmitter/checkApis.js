aba39a0c61cec164e2201e28ed0c9be5bda2eb55
Gleb Arestov <gr1b0k@gmail.com>
2016-09-04 19:53:35 +0300

    Remake `effect-` declaration, let to mark async way and results handling

--- js/libs/provoda/StatesEmitter/checkApis.js
+++ js/libs/provoda/StatesEmitter/checkApis.js
@@ -5,6 +5,8 @@ var spv = require('spv');
 var checkPrefix = require('./checkPrefix');
 var collectStatesBinders = require('./collectStatesBinders');
 
+var toRealArray = spv.toRealArray;
+
 var wrapDeps = function(deps) {
   if (typeof deps == 'string') {
     return [[deps]];
@@ -82,40 +84,54 @@ var ApiDeclr = function(name, data) {
   }
 };
 
+
+/*
+
+'effect-': [
+  [
+    ['апи'], ['инвалидирующие состояния'],
+    function (апи..., инвалидирующие состояния...) {
+
+    },
+    true || {state_name: 'response.field'}
+  ],
+  [['необходимые состояние'], ['необходимые эффекты']],
+],
+*/
+
 var ApiEffectDeclr = function(name, data) {
 
   this.name = name;
-  this.apis = spv.toRealArray(data[0]);
+  this.apis = null;
   this.triggering_states = null;
   this.deps = null;
   this.deps_name = null;
   this.effects_deps = null;
   this.fn = null;
+  this.result_schema = null;
+  this.is_async = null;
 
   this.compxes = null;
 
-  switch (data.length) {
-    case 3: {
-      this.triggering_states = data[1][0];
-      this.effects_deps = data[1][1];
-      this.fn = data[2];
-    }
-    break;
-    case 4: {
-      this.deps = wrapDeps(data[1]);
-      this.deps_name = '_need_api_effect_' + name;
+  var execution = data[0];
+  this.apis = toRealArray(execution[0]);
+  this.triggering_states = toRealArray(execution[1]);
+  this.fn = execution[2];
+  this.result_schema = execution[3];
+  this.is_async = !!execution[4];
 
-      this.compxes = [
-        this.deps_name, this.deps
-      ];
+  var condition = data[1];
+  var deps = condition && condition[0];
+  if (deps) {
+    this.deps = wrapDeps(deps);
+    this.deps_name = '_need_api_effect' + name;
 
-      this.triggering_states = data[2][0];
-      this.effects_deps = data[2][1];
-      this.fn = data[3];
-    }
-    break;
+    this.compxes = [
+      this.deps_name, this.deps
+    ];
   }
-
+  var effects_deps = condition && condition[1];
+  this.effects_deps = (effects_deps && toRealArray(effects_deps)) || null;
 };
 
 var checkApi = checkPrefix('api-', ApiDeclr, '__apis');
