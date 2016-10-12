d48048b3891e4504f5efee14a5bb863a091ca16c
Gleb Arestov <gr1b0k@gmail.com>
2016-10-12 22:20:27 +0300

    Refactor `encoded_states` cache writing

--- js/libs/provoda/utils/index.js
+++ js/libs/provoda/utils/index.js
@@ -23,61 +23,49 @@ var enc_states = {
 		return function parent(string) {
 			//example: '^visible'
 
-			if (!encoded_states[string]){
-				var state_name = string.replace(parent_count_regexp, '');
-				var count = string.length - state_name.length;
-				encoded_states[string] = {
-					rel_type: 'parent',
-					full_name: string,
-					ancestors: count,
-					state_name: state_name
-				};
-			}
-
-			return encoded_states[string];
+			var state_name = string.replace(parent_count_regexp, '');
+			var count = string.length - state_name.length;
+			return {
+				rel_type: 'parent',
+				full_name: string,
+				ancestors: count,
+				state_name: state_name
+			};
 		};
 	})(),
 	'@': function nesting(string) {
 		// nesting
 
 		//example:  '@some:complete:list'
-		if (!encoded_states[string]){
-			var nesting_and_state_name = string.slice(1);
-			var parts = nesting_and_state_name.split(':');
+		var nesting_and_state_name = string.slice(1);
+		var parts = nesting_and_state_name.split(':');
 
-			var nesting_name = parts.pop();
-			var state_name = parts.pop();
-			var zip_func = parts.pop();
+		var nesting_name = parts.pop();
+		var state_name = parts.pop();
+		var zip_func = parts.pop();
 
-			encoded_states[string] = {
-				rel_type: 'nesting',
-				full_name: string,
-				nesting_name: nesting_name,
-				state_name: state_name,
-				zip_func: zip_func || itself,
-				nwatch: new NestWatch(nesting_name.split('.'), state_name, zip_func, string)
-			};
-		}
-
-		return encoded_states[string];
+		return {
+			rel_type: 'nesting',
+			full_name: string,
+			nesting_name: nesting_name,
+			state_name: state_name,
+			zip_func: zip_func || itself,
+			nwatch: new NestWatch(nesting_name.split('.'), state_name, zip_func, string)
+		};
 	},
 	'#': function(string) {
 		// root
 
 		//example: '#vk_id'
-		if (!encoded_states[string]){
-			var state_name = string.slice(1);
-			if (!state_name) {
-				throw new Error('should be state_name');
-			}
-			encoded_states[string] = {
-				rel_type: 'root',
-				full_name: string,
-				state_name: state_name
-			};
+		var state_name = string.slice(1);
+		if (!state_name) {
+			throw new Error('should be state_name');
 		}
-
-		return encoded_states[string];
+		return {
+			rel_type: 'root',
+			full_name: string,
+			state_name: state_name
+		};
 	}
 };
 
@@ -86,7 +74,7 @@ function getEncodedState(state_name) {
 
 		var start = state_name.charAt(0);
 		if (enc_states[start]) {
-			enc_states[start](state_name);
+			encoded_states[state_name] = enc_states[start](state_name);
 		} else {
 			encoded_states[state_name] = null;
 		}
