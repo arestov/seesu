12aea46a53d9747cc6f42ee5337df3f06b6ca126
Gleb Arestov <gr1b0k@gmail.com>
2016-11-05 11:56:33 +0300

    Fix getting parents for nest_sel

--- js/libs/provoda/Model/addNestWatch.js
+++ js/libs/provoda/Model/addNestWatch.js
@@ -4,7 +4,9 @@ define(function (require) {
 var initDeclaredNestings = require('../initDeclaredNestings');
 
 return function addNestWatch(target, lnest_watch, skip) {
-  var start_md = lnest_watch.start_point ? initDeclaredNestings.get(target.app, target, lnest_watch.start_point) : target;
+  var start_md = lnest_watch.nwatch.start_point
+    ? initDeclaredNestings.getSPByPathTemplate(target.app, target, lnest_watch.nwatch.start_point)
+    : target;
   start_md.addNestWatch(lnest_watch, skip);
 };
 });
