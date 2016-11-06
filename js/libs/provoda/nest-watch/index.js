23a207af8fb75e75259c8f5cf379a2d97054d092
Gleb Arestov <gr1b0k@gmail.com>
2016-11-06 03:42:49 +0300

    Move `nest_sel` initing

--- js/libs/provoda/nest-watch/index.js
+++ js/libs/provoda/nest-watch/index.js
@@ -1,26 +1,16 @@
 define(function (require) {
 'use strict';
-var NestSelector = require('../StatesEmitter/NestSelector');
 var LocalWatchRoot = require('./LocalWatchRoot');
 var addFrom = require('./addFrom');
 var checkNesting = require('./checkNesting');
 var checkStates = require('./checkStates');
+var initNestSel = require('../StatesEmitter/initNestSel');
 
 function init(self) {
   self.states_links = null;
   self.nes_match_index = null;
 
-  if (self.nest_sel_nest_matches) {
-    for (var i = 0; i < self.nest_sel_nest_matches.length; i++) {
-      var cur = self.nest_sel_nest_matches[i];
-      var dest_w = new NestSelector(self, cur);
-      var source_w = new LocalWatchRoot(self, cur.nwbase, dest_w);
-      if (dest_w.state_name) {
-        addFrom(self, dest_w, 0);
-      }
-      addFrom(self, source_w, 0);
-    }
-  }
+  initNestSel(self);
 
   if (self.nest_match) {
     for (var i = 0; i < self.nest_match.length; i++) {
