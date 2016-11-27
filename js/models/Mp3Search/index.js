e974a2661f12db546b68a8b68c9c73f26bb20358
Gleb Arestov <gr1b0k@gmail.com>
2016-11-27 08:41:28 +0300

    Move `guessArtist`

--- js/models/Mp3Search/index.js
+++ js/models/Mp3Search/index.js
@@ -5,56 +5,8 @@ var spv = require('spv');
 var MusicFile = require('./MusicFile');
 var SongFileModel = require('../SongFileModel');
 var routePathByModels = require('js/libs/BrowseMap').routePathByModels;
+var guessArtist = require('./guessArtist');
 var pvState = pv.state;
-
-var guessArtist = function(track_title_raw, query_artist){
-	var track_title = track_title_raw.slice(0, 80);
-
-	var r = {};
-	if (!track_title){
-		return r;
-	}
-	var remove_digits = !query_artist || query_artist.search(/^\d+?\s?\S*?\s/) === 0;
-
-	if (remove_digits){
-		var matched_spaces = track_title.match(/\s?[\—\-\—\–]\s/gi);
-		if (matched_spaces && matched_spaces.length > 1){
-			track_title = track_title.replace(/^\d+[\s\.\—\-\—\–\_\|\+\(\)\*\&\!\?\@\,\\\/\❤\♡\'\"\[\]]*\s?/,"");
-		}
-		///^\d+[\s\.\—\-\—\–\_\|\+\(\)\*\&\!\?\@\,\\\/\❤\♡\'\"\[\]]*\s?/  for "813 - Elastique ( Rinse FM Rip )"
-
-		//01 The Killers - Song - ::remove number
-	}
-
-	var title_parts = track_title.split(/\s?[\—\-\—\–]\s/);
-	var artist_name_match = track_title.match(/([\s\S]*?)\s?[\—\-\—\–]\s/);
-	if (title_parts && title_parts.length > 1){
-		if (title_parts[0] == query_artist){
-			r.artist = artist_name_match[1];
-			r.track = track_title.replace(artist_name_match[0], '');
-		} else if (title_parts[title_parts.length-1] == query_artist){
-			var end_artist_name_match = track_title.match(/\s?[\—\-\—\–]\s([\s\S]*?)$/);
-			if (end_artist_name_match && end_artist_name_match[1]){
-				r.artist = end_artist_name_match[1];
-				r.track = track_title.replace(end_artist_name_match[0], '');
-			}
-		}
-	}
-	if (!r.artist){
-		var wordby_match = track_title.match(/by[\s]+?(.+)/);
-		if (query_artist && wordby_match && wordby_match[1] && wordby_match[1] == query_artist){
-			r.artist = query_artist;
-			r.track = track_title.replace(wordby_match[0], '');
-		} else if (title_parts && title_parts.length > 1){
-			r.artist = artist_name_match[1];
-			r.track = track_title.replace(artist_name_match[0], '');
-		} else if (query_artist && wordby_match){
-			r.artist = query_artist;
-			r.track = track_title.replace(wordby_match[0], '');
-		}
-	}
-	return r;
-};
 
 var QueryMatchIndex = function() {};
 
