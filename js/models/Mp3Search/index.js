21714deb1bf2475e54982ca34fc3acf7b4d31ed4
Gleb Arestov <gr1b0k@gmail.com>
2016-12-03 12:14:25 +0300

    Move `sortMusicFilesArray` fn

--- js/models/Mp3Search/index.js
+++ js/models/Mp3Search/index.js
@@ -6,6 +6,7 @@ var MusicFile = require('./MusicFile');
 var SongFileModel = require('../SongFileModel');
 var routePathByModels = require('js/libs/BrowseMap').routePathByModels;
 var guessArtist = require('./guessArtist');
+var sortMusicFilesArray = require('./sortMusicFilesArray');
 var pvState = pv.state;
 var QMI = require('./QMI');
 var getQueryString = QMI.getQueryString;
@@ -495,38 +496,6 @@ var hasMusicCopy = function (array, entity, from_position){
 	}
 };
 
-function calcSumm(arr) {
-  var summ = 0;
-  for (var i = 0; i < arr.length; i++) {
-    summ += arr[i];
-  }
-  return summ;
-}
-
-function getAvg(arr) {
-  if (!arr || !arr.length) {
-    return;
-  }
-  return calcSumm(arr)/arr.length;
-}
-
-var getAverageDurations = function(mu_array, time_limit, qmi_index){
-	var r = {};
-	var filtr = function(value){
-		if (value && value > time_limit){
-			return true;
-		}
-	};
-	for (var a in qmi_index){
-		var durs = spv.filter(spv.filter(qmi_index[a], 'duration', filtr), "duration");
-		r[a] = getAvg(durs);
-	}
-	return r;
-};
-
-
-
-
 	var Mp3Search = spv.inh(pv.Model, {
 		init: function(target, opts, data, searches_pr) {
 			// this._super.apply(this, arguments);
@@ -603,51 +572,8 @@ var getAverageDurations = function(mu_array, time_limit, qmi_index){
 			return this.tuners[search_name];
 		},
 		getQueryString: getQueryString,
-		sortMusicFilesArray: function(music_list, msq, time_limit) {
-			var searches_pr = pvState(this, 'searches_pr');
-
-			var query_string = getQueryString(msq);
-			time_limit = time_limit || 30000;
-
-			var field_name = ['query_match_index', query_string.replace(/\./gi, '')];
-			var qmi_index = spv.makeIndexByField(music_list, field_name);
-			var average_durs = getAverageDurations(music_list, time_limit, qmi_index);
-			music_list.sort(function(a, b){
-				return spv.sortByRules(a, b, [
-					function(item) {
-						var value = spv.getTargetField(item, field_name);
-						if (value === -1){
-							return Infinity;
-						} else {
-							return value;
-						}
-					},
-					{
-						field: function(item) {
-							if (item.from && searches_pr.hasOwnProperty(item.from) ) {
-								return searches_pr[item.from];
-							} else {
-								return -1000;
-							}
-						},
-						reverse: true
-					},
-					function(item){
-
-						var average_dur = average_durs[spv.getTargetField(item, field_name)];
-						if (average_dur){
-							if (item.duration && item.duration > time_limit){
-								return Math.abs(average_dur - item.duration);
-							} else {
-								return average_dur * 1000;
-							}
-						} else {
-							return Infinity;
-						}
-					}
-				]);
-			});
-
+		sortMusicFilesArray: function (music_list, msq, time_limit) {
+		  sortMusicFilesArray(this, music_list, msq, time_limit);
 		},
 		getFileQMI: getFileQMI,
 		setFileQMI: setFileQMI,
---
+++ js/models/Mp3Search/sortMusicFilesArray.js
@@ -0,0 +1,88 @@
+define(function (require) {
+'use strict';
+var pv = require('pv');
+var spv = require('spv');
+var pvState = pv.state;
+var QMI = require('./QMI');
+var getQueryString = QMI.getQueryString;
+
+function calcSumm(arr) {
+  var summ = 0;
+  for (var i = 0; i < arr.length; i++) {
+    summ += arr[i];
+  }
+  return summ;
+}
+
+function getAvg(arr) {
+  if (!arr || !arr.length) {
+    return;
+  }
+  return calcSumm(arr)/arr.length;
+}
+
+var getAverageDurations = function(mu_array, time_limit, qmi_index){
+	var r = {};
+	var filtr = function(value){
+		if (value && value > time_limit){
+			return true;
+		}
+	};
+	for (var a in qmi_index){
+		var durs = spv.filter(spv.filter(qmi_index[a], 'duration', filtr), "duration");
+		r[a] = getAvg(durs);
+	}
+	return r;
+};
+
+ function sortMusicFilesArray(mp3_search, music_list, msq, time_limit) {
+  var searches_pr = pvState(mp3_search, 'searches_pr');
+
+  var query_string = getQueryString(msq);
+  time_limit = time_limit || 30000;
+
+  var field_name = ['query_match_index', query_string.replace(/\./gi, '')];
+  var qmi_index = spv.makeIndexByField(music_list, field_name);
+  var average_durs = getAverageDurations(music_list, time_limit, qmi_index);
+
+  music_list.sort(function(a, b){
+    return spv.sortByRules(a, b, [
+      function(item) {
+        var value = spv.getTargetField(item, field_name);
+        if (value === -1){
+          return Infinity;
+        } else {
+          return value;
+        }
+      },
+      {
+        field: function(item) {
+          if (item.from && searches_pr.hasOwnProperty(item.from) ) {
+            return searches_pr[item.from];
+          } else {
+            return -1000;
+          }
+        },
+        reverse: true
+      },
+      function(item){
+
+        var average_dur = average_durs[spv.getTargetField(item, field_name)];
+        if (average_dur){
+          if (item.duration && item.duration > time_limit){
+            return Math.abs(average_dur - item.duration);
+          } else {
+            return average_dur * 1000;
+          }
+        } else {
+          return Infinity;
+        }
+      }
+    ]);
+  });
+}
+
+sortMusicFilesArray.getAvg = getAvg;
+
+return sortMusicFilesArray;
+});
