920706df2f0dbc02d4bd3eb6c980164890622611
Gleb Arestov <gr1b0k@gmail.com>
2016-12-07 11:16:49 +0300

    Move `Source` to fn

---
+++ js/models/Mp3Search/Sources/createSource.js
@@ -0,0 +1,40 @@
+define(function (require) {
+'use strict';
+var pv = require('pv');
+var MusicFile = require('../MusicFile');
+
+var BlankQuery = pv.behavior({
+  // 'api-eng': function () {
+  //   return null;
+  // },
+  requestFiles: function () {
+    console.log('BlankQuery requesting');
+  },
+});
+
+return function (Query, File) {
+  return pv.behavior({
+    sub_pager: {
+      type: {
+        queries: 'query',
+        files: 'file'
+      },
+      by_type: {
+        file: [
+          File || MusicFile, null, {
+            id: 'by_slash.0'
+          }
+        ],
+        query: [
+          Query || BlankQuery, null, {
+            artist_name: 'by_comma.0',
+            track_title: 'by_comma.1',
+            'msq.artist': 'by_comma.0',
+            'msq.track': 'by_comma.1',
+          }
+        ],
+      }
+    }
+  });
+};
+});
--- js/models/Mp3Search/Sources/index.js
+++ js/models/Mp3Search/Sources/index.js
@@ -1,28 +1,12 @@
 define(function (require) {
 'use strict';
 var pv = require('pv');
-var MusicFile = require('../MusicFile');
-
-var Source = pv.behavior({
-  sub_pager: {
-    type: {
-      files: 'file'
-    },
-    by_type: {
-      file: [
-        MusicFile, null, {
-          id: 'by_slash.0'
-        }
-      ],
-    }
-  }
-});
-
+var createSource = require('./createSource');
 
 return pv.behavior({
   sub_pager: {
     item: [
-      Source,
+      createSource(),
       [[]],
       {
         search_name: 'decoded_name'
