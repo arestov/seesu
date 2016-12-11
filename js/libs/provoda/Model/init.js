df64d139f3320588447f575caecfb4e567dc4d81
Gleb Arestov <gr1b0k@gmail.com>
2016-12-11 14:33:56 +0300

    Move `initModel`

---
+++ js/libs/provoda/Model/init.js
@@ -0,0 +1,123 @@
+define(function (require) {
+'use strict';
+
+var cloneObj = require('spv').cloneObj;
+
+return function initModel(self, opts, data, params, more, states) {
+  self.current_motivator = self.current_motivator || (opts && opts._motivator);
+
+  if (opts && opts.app){
+    self.app = opts.app;
+  }
+
+  self.app = self.app || null;
+
+  if (opts._highway) {
+    self._highway = opts._highway;
+  }
+
+  if (!self._highway) {
+    self._highway = self.app._highway;
+  }
+
+  self._highway = self._highway || null;
+
+  self._calls_flow = self._highway.calls_flow;
+
+  self.sub_pages = null;
+
+  if (self._sub_pages || self._sub_pager){
+    self.sub_pages = {};
+  }
+
+  if (opts && opts.map_parent){
+    self.map_parent = opts.map_parent;
+  }
+
+  self.map_parent = self.map_parent || null;
+
+  self.req_order_field = null;
+
+  self._provoda_id = self._highway.models_counters++;
+  self._highway.models[self._provoda_id] = self;
+
+  //self.states = {};
+
+  self.children_models = null;
+  self._network_source = self._network_source || null;
+
+
+  self.md_replacer = null;
+  self.mpx = null;
+  self._requests_deps = null;
+  self.shared_nest_sel_hands = null;
+
+  self.init_states = self.init_states || null;
+
+  self.init_service_states = null;
+
+  if (states || (data && data.states)) {
+
+    toServStates(self, states);
+    toServStates(self, data && data.states);
+    // pv.create must init init_states
+  }
+
+  self.head = null;
+
+  if (self.map_parent && self.map_parent.head) {
+    if (!self.head) {self.head = {};}
+    cloneObj(self.head, self.map_parent.head);
+  }
+
+  if (data && data.head) {
+    if (!self.head) {self.head = {};}
+    cloneObj(self.head, data.head);
+  }
+
+
+  if (self.network_data_as_states && data && data.network_states) {
+    toServStates(self, data.network_states);
+
+    if (self.net_head) {
+      if (!self.head) {self.head = {};}
+      for (var i = 0; i < self.net_head.length; i++) {
+        var pk = self.net_head[i];
+        self.head[pk] = data.network_states[pk];
+      }
+    }
+  }
+
+  if (self.head) {
+    toServStates(self, self.head);
+  }
+
+  if (!self.init_service_states) {
+    return self;
+  }
+
+  for (var state_name in self.init_service_states) {
+    if (self.hasComplexStateFn(state_name)) {
+      delete self.init_service_states[state_name];
+    }
+  }
+
+  self.init_states = self.init_states || {};
+
+  cloneObj(self.init_states, self.init_service_states);
+  self.init_service_states = null;
+
+
+  return self;
+};
+
+function toServStates(self, states) {
+  if (!states) {return;}
+
+  if (!self.init_service_states) {
+    self.init_service_states = {};
+  }
+
+  cloneObj(self.init_service_states, states);
+}
+});
