65d863c236cb5f2ba121a3034ed0b7ef82e72061
Gleb Arestov <gr1b0k@gmail.com>
2016-12-18 08:21:27 +0300

    Fix code style

--- js/libs/ScApi.js
+++ js/libs/ScApi.js
@@ -1,6 +1,10 @@
-define(['spv', 'js/modules/aReq', 'js/modules/wrapRequest', 'js/modules/extendPromise', 'hex_md5'],
-function(spv, aReq, wrapRequest, extendPromise, hex_md5) {
+define(function(require) {
 'use strict';
+var spv = require('spv');
+var aReq = require('js/modules/aReq');
+var wrapRequest = require('js/modules/wrapRequest');
+var hex_md5 = require('hex_md5');
+
 var ScApi = function(key, queue, crossdomain, cache_ajax) {
 	this.queue = queue;
 	this.key = key;
@@ -15,46 +19,45 @@ ScApi.prototype = {
 	cache_namespace: "soundcloud_api",
 	thisOriginAllowed: true,
 	get: function(method, params, options) {
+		if (!method) {return;}
 
-		if (method) {
-			options = options || {};
-			params = params || {};
+    options = options || {};
+    params = params || {};
 
-			if (options && options.paging) {
-				params.limit = options.paging.page_limit;
-				params.offset = options.paging.page_limit * (options.paging.next_page -1);
-			}
+    if (options && options.paging) {
+      params.limit = options.paging.page_limit;
+      params.offset = options.paging.page_limit * (options.paging.next_page -1);
+    }
 
-			params.consumer_key = this.key;
+    params.consumer_key = this.key;
 
-			options.cache_key = options.cache_key || hex_md5("http://api.soundcloud.com/" + method + spv.stringifyParams(params));
+    options.cache_key = options.cache_key || hex_md5("http://api.soundcloud.com/" + method + spv.stringifyParams(params));
 
-			var wrap_def = wrapRequest({
-				url: "http://api.soundcloud.com/" + method + ".json",
-				type: "GET",
-				dataType: this.crossdomain ? "json": "jsonp",
-				data: params,
-				timeout: 20000,
-				resourceCachingAvailable: true,
-				afterChange: function(opts) {
-					if (opts.dataType == 'json'){
-						opts.headers = null;
-					}
-				},
-				thisOriginAllowed: this.thisOriginAllowed,
-				context: options.context
-			}, {
-				cache_ajax: this.cache_ajax,
-				nocache: options.nocache,
-				cache_key: options.cache_key,
-				cache_timeout: options.cache_timeout,
-				cache_namespace: this.cache_namespace,
-				requestFn: aReq,
-				queue: this.queue
-			});
+    var wrap_def = wrapRequest({
+      url: "http://api.soundcloud.com/" + method + ".json",
+      type: "GET",
+      dataType: this.crossdomain ? "json": "jsonp",
+      data: params,
+      timeout: 20000,
+      resourceCachingAvailable: true,
+      afterChange: function(opts) {
+        if (opts.dataType == 'json'){
+          opts.headers = null;
+        }
+      },
+      thisOriginAllowed: this.thisOriginAllowed,
+      context: options.context
+    }, {
+      cache_ajax: this.cache_ajax,
+      nocache: options.nocache,
+      cache_key: options.cache_key,
+      cache_timeout: options.cache_timeout,
+      cache_namespace: this.cache_namespace,
+      requestFn: aReq,
+      queue: this.queue
+    });
 
-			return wrap_def.complex;
-		}
+    return wrap_def.complex;
 	}
 };
 
