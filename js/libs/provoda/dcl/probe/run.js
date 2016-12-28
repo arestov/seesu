ccd29c12f2e816fb1d05760b135dcb92017b6a89
Gleb Arestov <gr1b0k@gmail.com>
2016-12-28 07:41:02 +0300

    Create `probe` models

--- js/libs/provoda/dcl/probe/run.js
+++ js/libs/provoda/dcl/probe/run.js
@@ -1,20 +1,63 @@
 define(function (require) {
 'use strict';
-// var pv = require('pv');
-// var Probe = pv.behavior({}, null, {
-//   naming: function(fn) {
-// 		return function Probe(opts, data, params, more, states) {
-// 			fn(this, opts, data, params, more, states);
-// 		};
-// 	},
-// });
+var spv = require('spv');
+
+var RunProbes = function () {
+  this.list = [];
+  this.index = {};
+  this.grouped = {};
+};
 
 return function (bwlev, pathp) {
-  if (!pathp.md._run_probes) {
-    pathp.md._run_probes = {};
+  var md = pathp.md;
+  if (!md._run_probes) {
+    md._run_probes = new RunProbes();
   }
-  // for
-  console.log('!!!!!!!!!!!!_________________')
+
+  var set = md._run_probes;
+  var app = bwlev.app;
+  var has_changes = false;
+
+  for (var name in md._probs) {
+    if (!md._probs.hasOwnProperty(name)) {continue;}
+    var cur = md._probs[name];
+
+    if (!canCreateProbe(bwlev, pathp, cur)) {continue;}
+
+    var key = name;
+
+    // contains
+    // get
+    // add
+    // remove
+
+    if (spv.set.contains(set, key)) {continue;}
+
+    var con = app.initChi('__probe', null, null, null, {
+      path: pathp.path,
+      name: cur.name,
+    });
+    spv.set.add(set, key, con);
+
+    set.grouped[cur.name] = set.grouped[cur.name] || [];
+    set.grouped[cur.name].push(con);
+
+    has_changes = true;
+  }
+
+  if (!has_changes) {return;}
+
+  for (var name in set.grouped) {
+    if (!set.grouped.hasOwnProperty(name)) {continue;}
+
+    md.updateNesting('__run_probe_' + name, set.grouped[name]);
+  }
+
 };
+
+
+function canCreateProbe(bwlev, pathp, dcl) {
+  return true;
+}
 
 });
--- js/libs/provoda/structure/prepare.js
+++ js/libs/provoda/structure/prepare.js
@@ -1,9 +1,21 @@
 define(function (require) {
 'use strict';
+var Model = require('../Model');
 var mark = require('./mark');
+var spv = require('spv');
+
+var Probe = spv.inh(Model, {
+  naming: function(fn) {
+		return function Probe(opts, data, params, more, states) {
+			fn(this, opts, data, params, more, states);
+		};
+	},
+});
 
 return function prepare(root) {
-  var augmented = root;
+  var augmented = spv.inh(root, {}, {
+    'chi-__probe': Probe
+  });
   return mark(augmented, augmented);
 };
 
