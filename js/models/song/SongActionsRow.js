970da8c0a8b73eff60092a962e2a8d2575597c8f
Gleb Arestov <gr1b0k@gmail.com>
2017-01-09 10:19:15 +0300

    Use `steps_to_surface_limit`

--- js/models/song/SongActionsRow.js
+++ js/models/song/SongActionsRow.js
@@ -151,7 +151,13 @@ var SongActionsRow = spv.inh(comd.PartsSwitcher, {
       }
     ]
   },
-
+  'probe-song_action': {
+    main: ['nesting', {
+      place_of_current: 'action',
+      place_of_nav: null,
+    }],
+    steps_to_surface_limit: 1,
+  },
   sub_page: parts_storage,
   'nest_posb-context_parts': constrs_names,
 
