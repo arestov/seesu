fd376ee71550d6e2608607ef7d332fc5769434d8
Gleb Arestov <gr1b0k@gmail.com>
2017-03-12 10:49:12 +0300

    Fix code style

--- js/libs/BrowseMap.js
+++ js/libs/BrowseMap.js
@@ -1254,48 +1254,49 @@ BrowseMap.getStruc = (function() {
 		var dclrs_selectors = used_data_structure.collch_selectors;
 
 		var bwlev_dclr = pv.$v.selecPoineertDeclr(dclrs_fpckgs, dclrs_selectors, 'map_slice', model_name, 'main', true);
-		if (bwlev_dclr) {
-			var path_mod = 'm_children.children.map_slice.' + (bwlev_dclr.space || 'main');
-
-			//+ '.m_children.children_by_mn.pioneer';
-			var bwlev_struc = spv.getTargetField(used_data_structure, path_mod);
-			var bwlev_dclrs_fpckgs = bwlev_struc.collch_dclrs;
-			var bwlev_dclrs_selectors = bwlev_struc.collch_selectors;
-
-			var pioneer_model_name = bwlev_dclr.is_wrapper_parent ? md.map_parent.model_name : model_name;
-			var md_dclr = pv.$v.selecPoineertDeclr(bwlev_dclrs_fpckgs, bwlev_dclrs_selectors, 'pioneer', pioneer_model_name, (bwlev_dclr.space || 'main'), true);
-
-			var children = spv.getTargetField(bwlev_struc, children_path);
-
-			struc = spv.getTargetField(children, [pioneer_model_name, md_dclr.space]) || spv.getTargetField(children, ['$default', md_dclr.space]);
-
-			if (!bwlev_dclr.is_wrapper_parent) {
-				return struc;
-			} else {
-				var nestings = struc.m_children.children;
-				var Constr = md.constructor;
-				for (var nesting_name in nestings) {
-					var items = BrowseMap.getNestingConstr(app, md.map_parent, nesting_name);
-					if (items) {
-						if (Array.isArray(items)) {
-							if (items.indexOf(Constr) != -1) {
-								struc = nestings[nesting_name];
-								break;
-							}
-						} else {
-							if (items == Constr) {
-								struc = nestings[nesting_name];
-								break;
-							}
-						}
-					}
-				}
-				return struc;
-			}
-		} else {
+		if (!bwlev_dclr) {
 			var default_struc = spv.getTargetField(used_data_structure, path)[ '$default' ];
 			return spv.getTargetField(used_data_structure, path)[ model_name ] || default_struc;
 		}
+
+		var path_mod = 'm_children.children.map_slice.' + (bwlev_dclr.space || 'main');
+
+		//+ '.m_children.children_by_mn.pioneer';
+		var bwlev_struc = spv.getTargetField(used_data_structure, path_mod);
+		var bwlev_dclrs_fpckgs = bwlev_struc.collch_dclrs;
+		var bwlev_dclrs_selectors = bwlev_struc.collch_selectors;
+
+		var pioneer_model_name = bwlev_dclr.is_wrapper_parent ? md.map_parent.model_name : model_name;
+		var md_dclr = pv.$v.selecPoineertDeclr(bwlev_dclrs_fpckgs, bwlev_dclrs_selectors, 'pioneer', pioneer_model_name, (bwlev_dclr.space || 'main'), true);
+
+		var children = spv.getTargetField(bwlev_struc, children_path);
+
+		struc = spv.getTargetField(children, [pioneer_model_name, md_dclr.space]) || spv.getTargetField(children, ['$default', md_dclr.space]);
+
+		if (!bwlev_dclr.is_wrapper_parent) {
+			return struc;
+		}
+
+		var nestings = struc.m_children.children;
+		var Constr = md.constructor;
+		for (var nesting_name in nestings) {
+			var items = BrowseMap.getNestingConstr(app, md.map_parent, nesting_name);
+			if (items) {
+				if (Array.isArray(items)) {
+					if (items.indexOf(Constr) != -1) {
+						struc = nestings[nesting_name];
+						break;
+					}
+				} else {
+					if (items == Constr) {
+						struc = nestings[nesting_name];
+						break;
+					}
+				}
+			}
+		}
+		return struc;
+
 	}, function(md){
 		return md.constr_id;
 	});
