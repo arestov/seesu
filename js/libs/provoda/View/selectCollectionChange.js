7add33376b7f929f828d1b4a3d409cc1aebe6fff
Gleb Arestov <gr1b0k@gmail.com>
2017-03-31 23:00:33 +0300

    RF Fix code style

--- js/libs/provoda/View/selectCollectionChange.js
+++ js/libs/provoda/View/selectCollectionChange.js
@@ -10,35 +10,39 @@ return function (target, nesname, items, removed, old_value) {
   var collch = target.dclrs_fpckgs && target.dclrs_fpckgs.hasOwnProperty(nesname) && target.dclrs_fpckgs[nesname];
   if (typeof collch == 'function') {
     target.callCollectionChangeDeclaration(collch, nesname, items, old_value, removed);
-  } else {
-    if (target.dclrs_selectors && target.dclrs_selectors.hasOwnProperty(nesname)) {
-      if (Array.isArray(items)) {
-        for (var i = 0; i < items.length; i++) {
-          var cur = items[i];
-          var dclr = $v.selecPoineertDeclr(target.dclrs_fpckgs, target.dclrs_selectors,
-            nesname, cur.model_name, target.nesting_space);
+    return;
+  }
 
-          if (!dclr) {
-            dclr = collch;
-          }
-
-          throw new Error('WHAT TO DO WITH old_value?');
-          // target.callCollectionChangeDeclaration(dclr, nesname, cur, old_value, removed);
-        }
-      } else {
-        var dclr = $v.selecPoineertDeclr(target.dclrs_fpckgs, target.dclrs_selectors,
-            nesname, items.model_name, target.nesting_space);
-
-        if (!dclr) {
-          dclr = collch;
-        }
-        target.callCollectionChangeDeclaration(dclr, nesname, items, old_value, removed);
-      }
-    } else {
-      if (collch) {
-        target.callCollectionChangeDeclaration(collch, nesname, items, old_value, removed);
-      }
+  if (!target.dclrs_selectors || !target.dclrs_selectors.hasOwnProperty(nesname)) {
+    if (!collch) {
+      return;
     }
+
+    target.callCollectionChangeDeclaration(collch, nesname, items, old_value, removed);
+    return;
+  }
+
+  if (!Array.isArray(items)) {
+    var dclr = $v.selecPoineertDeclr(
+      target.dclrs_fpckgs, target.dclrs_selectors,
+      nesname, items.model_name, target.nesting_space
+    ) || collch;
+
+    target.callCollectionChangeDeclaration(dclr, nesname, items, old_value, removed);
+    return;
+  }
+
+  for (var i = 0; i < items.length; i++) {
+    var cur = items[i];
+    var dclr = $v.selecPoineertDeclr(target.dclrs_fpckgs, target.dclrs_selectors,
+      nesname, cur.model_name, target.nesting_space);
+
+    if (!dclr) {
+      dclr = collch;
+    }
+
+    throw new Error('WHAT TO DO WITH old_value?');
+    // target.callCollectionChangeDeclaration(dclr, nesname, cur, old_value, removed);
   }
 }
 });
