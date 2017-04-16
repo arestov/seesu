8cd65037b37253cd8c1c918b79c7fd95db87bbfa
Gleb Arestov <gr1b0k@gmail.com>
2017-04-16 12:00:09 +0300

    RF Move code

--- js/views/AppView.js
+++ js/views/AppView.js
@@ -653,21 +653,6 @@ var AppView = spv.inh(AppBaseView.WebComplexTreesView, {}, {
 		}
 	},
 
-	appendStyle: function(style_text){
-		//fixme - check volume ondomready
-		var style_node = this.d.createElement('style');
-			style_node.setAttribute('title', 'button_menu');
-			style_node.setAttribute('type', 'text/css');
-
-		if (!style_node.styleSheet){
-			style_node.appendChild(this.d.createTextNode(style_text));
-		} else{
-			style_node.styleSheet.cssText = style_text;
-		}
-
-		this.d.documentElement.firstChild.appendChild(style_node);
-
-	},
 	trackEvent: function() {
 		var args = Array.prototype.slice.apply(arguments);
 		args.unshift('trackEvent');
