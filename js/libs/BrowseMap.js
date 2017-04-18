47ebac0aa05ee6358e508685638083a5e904cc5d
Gleb Arestov <gr1b0k@gmail.com>
2017-04-18 21:41:35 +0300

    RF Move `getModelSources`

--- js/libs/BrowseMap.js
+++ js/libs/BrowseMap.js
@@ -6,6 +6,7 @@ var get_constr = require('./provoda/structure/get_constr');
 var flatStruc = require('./provoda/structure/flatStruc');
 var routePathByModels = require('./provoda/routePathByModels');
 var getUsageStruc = require('./provoda/structure/getUsageStruc');
+var getModelSources = require('./provoda/structure/getModelSources');
 
 var getSPIConstr = routePathByModels.getSPIConstr;
 var getSPI= routePathByModels.getSPI;
@@ -13,9 +14,7 @@ var getSPI= routePathByModels.getSPI;
 var pvState = pv.state;
 var cloneObj = spv.cloneObj;
 var countKeys = spv.countKeys;
-var collapseAll = spv.collapseAll;
 
-var getNestingConstr = get_constr.getNestingConstr;
 var getDeclrConstr = get_constr.getDeclrConstr;
 
 /*
@@ -648,104 +647,7 @@ BrowseMap.getUserInterest = function(pth_string, start_md) {
 
 BrowseMap.routePathByModels = routePathByModels;
 
-BrowseMap.getNestingConstr = getNestingConstr;
 BrowseMap.getDeclrConstr = getDeclrConstr;
-
-var getModelSources = function(app, md, cur) {
-	var states_sources = [];
-	var i;
-	var states_list = cur.merged_states;
-	var unfolded_states = new Array(states_list.length);
-	for (i = 0; i < states_list.length; i++) {
-		unfolded_states[i] = md.getNonComplexStatesList(states_list[i]);
-	}
-
-	unfolded_states = collapseAll.apply(null, unfolded_states);
-
-	for (i = 0; i < unfolded_states.length; i++) {
-		var state_name = unfolded_states[i];
-		var arr = md.getStateSources(state_name, app);
-		if (arr) {
-			states_sources.push(arr);
-		}
-
-
-	}
-	states_sources = collapseAll.apply(null, states_sources);
-
-	var nestings_names_list = [];
-
-	var nesting_name;
-	for (nesting_name in cur.m_children.children_by_mn) {
-		nestings_names_list.push(nesting_name);
-	}
-	for (nesting_name in cur.m_children.children) {
-		nestings_names_list.push(nesting_name);
-	}
-
-	nestings_names_list = collapseAll(nestings_names_list);
-
-	var nesting_sources = [];
-	for (i = 0; i < nestings_names_list.length; i++) {
-		var source = md.getNestingSource(nestings_names_list[i], app);
-		if (source) {
-			nesting_sources.push(source);
-		}
-	}
-
-
-	var all_nest_sources =[];
-
-	for (nesting_name in cur.m_children.children) {
-		var items = getNestingConstr(app, md, nesting_name);
-		for (var space_name in cur.m_children.children[nesting_name]) {
-
-			var constr_sources;
-			if (!items) {
-				continue;
-			}
-			if (Array.isArray(items)) {
-				constr_sources = [];
-				for (i = 0; i < items.length; i++) {
-					var cur_sources = getModelSources(app, items[i].prototype, cur.m_children.children[nesting_name][space_name]);
-					if (cur_sources.length) {
-						constr_sources = constr_sources.concat(cur_sources);
-					}
-				}
-			} else {
-				constr_sources = getModelSources(app, items.prototype, cur.m_children.children[nesting_name][space_name]);
-			}
-
-			if (constr_sources) {
-				all_nest_sources = all_nest_sources.concat(constr_sources);
-			}
-		}
-
-	}
-
-
-
-
-
-	/*
-	a) итерируем по названиям гнезд,
-		получаем список или один конструктор для нужного гнезда
-		совмещаем данные
-
-	б) итерируем по названиям гнезд
-		получаем список или один конструктор для нужного гнезда
-		вычленяем по имени модели только используемые конструкторы
-
-
-	*/
-
-	var full_sources_list = states_sources.concat(nesting_sources);
-	if (all_nest_sources.length) {
-		full_sources_list = full_sources_list.concat(all_nest_sources);
-	}
-	return  collapseAll(full_sources_list);
-};
-
 
 BrowseMap.getStrucSources = function(md, struc) {
 	//console.log(struc);
---
+++ js/libs/provoda/structure/getModelSources.js
@@ -0,0 +1,104 @@
+define(function(require) {
+'use strict';
+var spv = require('spv');
+var get_constr = require('./get_constr');
+
+var collapseAll = spv.collapseAll;
+var getNestingConstr = get_constr.getNestingConstr;
+
+
+return function getModelSources(app, md, cur) {
+	var states_sources = [];
+	var i;
+	var states_list = cur.merged_states;
+	var unfolded_states = new Array(states_list.length);
+	for (i = 0; i < states_list.length; i++) {
+		unfolded_states[i] = md.getNonComplexStatesList(states_list[i]);
+	}
+
+	unfolded_states = collapseAll.apply(null, unfolded_states);
+
+	for (i = 0; i < unfolded_states.length; i++) {
+		var state_name = unfolded_states[i];
+		var arr = md.getStateSources(state_name, app);
+		if (arr) {
+			states_sources.push(arr);
+		}
+
+
+	}
+	states_sources = collapseAll.apply(null, states_sources);
+
+	var nestings_names_list = [];
+
+	var nesting_name;
+	for (nesting_name in cur.m_children.children_by_mn) {
+		nestings_names_list.push(nesting_name);
+	}
+	for (nesting_name in cur.m_children.children) {
+		nestings_names_list.push(nesting_name);
+	}
+
+	nestings_names_list = collapseAll(nestings_names_list);
+
+	var nesting_sources = [];
+	for (i = 0; i < nestings_names_list.length; i++) {
+		var source = md.getNestingSource(nestings_names_list[i], app);
+		if (source) {
+			nesting_sources.push(source);
+		}
+	}
+
+
+	var all_nest_sources =[];
+
+	for (nesting_name in cur.m_children.children) {
+		var items = getNestingConstr(app, md, nesting_name);
+		for (var space_name in cur.m_children.children[nesting_name]) {
+
+			var constr_sources;
+			if (!items) {
+				continue;
+			}
+			if (Array.isArray(items)) {
+				constr_sources = [];
+				for (i = 0; i < items.length; i++) {
+					var cur_sources = getModelSources(app, items[i].prototype, cur.m_children.children[nesting_name][space_name]);
+					if (cur_sources.length) {
+						constr_sources = constr_sources.concat(cur_sources);
+					}
+				}
+			} else {
+				constr_sources = getModelSources(app, items.prototype, cur.m_children.children[nesting_name][space_name]);
+			}
+
+			if (constr_sources) {
+				all_nest_sources = all_nest_sources.concat(constr_sources);
+			}
+		}
+
+	}
+
+
+
+
+
+	/*
+	a) итерируем по названиям гнезд,
+		получаем список или один конструктор для нужного гнезда
+		совмещаем данные
+
+	б) итерируем по названиям гнезд
+		получаем список или один конструктор для нужного гнезда
+		вычленяем по имени модели только используемые конструкторы
+
+
+	*/
+
+	var full_sources_list = states_sources.concat(nesting_sources);
+	if (all_nest_sources.length) {
+		full_sources_list = full_sources_list.concat(all_nest_sources);
+	}
+	return  collapseAll(full_sources_list);
+};
+});
