9013b534881381f7aa6a5bc4b9b11fc5fcbdfbf9
Gleb Arestov <gr1b0k@gmail.com>
2017-11-05 10:24:43 +0300

    Fix url generating

---
+++ js/libs/provoda/bwlev/getNavGroups.js
@@ -0,0 +1,25 @@
+define(function(require) {
+'use strict';
+var isBigStep = require('./isBigStep');
+
+return function getNavGroups(bwlev) {
+	var cur_group = [];
+	var groups = [cur_group];
+
+	var cur = bwlev;
+	var cur_child = cur.getNesting('pioneer');
+	while (cur) {
+		cur_group.push(cur_child);
+
+		if (isBigStep(cur, cur_child)) {
+			cur_group = [];
+			groups.push(cur_group);
+		}
+
+		cur = cur.map_parent;
+		cur_child = cur && cur.getNesting('pioneer');
+	}
+	return groups;
+};
+
+})
---
+++ js/libs/provoda/bwlev/joinNavURL.js
@@ -0,0 +1,50 @@
+define(function(require) {
+'use strict';
+var getNavGroups = require('./getNavGroups');
+
+var joinSubtree = function(array){
+	var url = "";
+	for (var i = array.length - 1; i >= 0; i--) {
+		var md = 	array[i];
+		var url_part = md.state('url_part');
+		// if (!url_part) {
+		// 	throw new Error('must be url');
+		// }
+		url += url_part || '';
+	}
+	return url;
+};
+
+return function(nav) {
+	if (!nav || !nav.length) {
+		return null;
+	}
+
+	var url = '';
+
+
+	var groups = getNavGroups(nav[ nav.length - 1 ]);
+
+	/*
+		/users/me/lfm:neighbours#3:/users/lfm:kolczyk0
+	*/
+
+	var last = groups.pop();
+
+	url += joinSubtree(last);
+
+
+	for (var i = groups.length - 1; i >= 0; i--) {
+		var distance = groups[i].length;
+		url += '#';
+
+		if (distance > 1) {
+			url += distance + ':';
+		}
+
+		url += joinSubtree(groups[i]);
+	}
+
+	return url;
+};
+});
