34a8b4ab3ffb68e7721af6b9d6af7ce0fd71978e
Gleb Arestov <gr1b0k@gmail.com>
2018-04-17 22:14:10 +0300

    Remove dead code

--- js/initBrowsing.js
+++ js/initBrowsing.js
@@ -1,18 +1,11 @@
 define(function(require) {
 'use strict';
-var pv = require('pv');
-var navi = require('./libs/navi');
 var BrowseMap = require('./libs/BrowseMap');
-var animateMapChanges = require('js/libs/provoda/dcl/probe/animateMapChanges');
-var app_serv = require('app_serv');
-
-var pvUpdate = pv.update;
-var app_env = app_serv.app_env;
 
 return function initBrowsing(app) {
-  var map = BrowseMap.hookRoot(app, app.start_page);
-  app.map = map;
-  return map;
+  var bwroot = BrowseMap.hookRoot(app, app.start_page);
+  app.bwroot = bwroot;
+  return bwroot;
 };
 
 });
