05070c085cbc055392793a94bcbe5a41360cda96
Gleb Arestov <gr1b0k@gmail.com>
2018-04-19 10:01:28 +0300

    Use sub pages for spyglasses

--- js/libs/provoda/bwlev/RootLev.js
+++ js/libs/provoda/bwlev/RootLev.js
@@ -15,43 +15,9 @@ var getModelById = require('../utils/getModelById');
 var followFromTo = require('./followFromTo');
 var getSPByPathTemplate = require('../initDeclaredNestings').getSPByPathTemplate;
 
-var Probe = spv.inh(Model, {
-  naming: function(fn) {
-		return function Probe(opts, data, params, more, states) {
-			fn(this, opts, data, params, more, states);
-		};
-	},
-  init: function(self) {
-    self.bwlevs = {};
-  },
-}, {
-  'stch-used_struc': function(self, value) {
-    console.log('GOT used_struc', value);
-  },
-  '+states': {
-    struc: [
-      "compx", ['used_struc', '@current_md', 'name'],
-  		function(struc, pioneer, probe_name) {
-  			// if (num == -2) {return}
-  			if (!struc || !pioneer || !probe_name) {return;}
-  			return getUsageStruc(pioneer, probe_name, struc, this.app);
-  		}
-  	],
-  }
-  // 'compx-struc': [
-  //   ['@one:struc:owner_bwlev', 'name'],
-  //   function(struc, name) {
-  //     if (!struc) {return;}
-  //
-  //     console.log('---------Probe', name, struc.main.m_children.children);
-  //     return struc.main.m_children[name];
-  //   }
-  // ]
-});
 
 var RootLev = spv.inh(Model, {}, {
   BWL: BrowseLevel,
-  'chi-__probe': Probe,
   rpc_legacy: {
     requestSpyglass: handleSpyglassRequests,
     requestPage: function(id) {
