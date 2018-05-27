95fd221edead3df4dea28612e2f6bff41e210e28
Gleb Arestov <gr1b0k@gmail.com>
2018-05-27 11:04:36 +0300

    Refactor nest_conj/check

--- js/libs/provoda/dcl/nest_conj/check.js
+++ js/libs/provoda/dcl/nest_conj/check.js
@@ -2,17 +2,15 @@ define(function(require) {
 'use strict';
 var checkPrefix = require('../../StatesEmitter/checkPrefix');
 var NestCntDeclr = require('./item')
-var checkApi = checkPrefix('nest_conj-', NestCntDeclr, '_chi_nest_conj');
-
-return function check(self, props) {
-  var cnts = checkApi(self, props);
-  if (!cnts) {return;}
-
+var handle = function(self, cnts) {
   self.nest_concat_nest_matches = [];
 
   for (var res in cnts) {
     self.nest_concat_nest_matches.push(cnts[res]);
   }
-};
+}
+var checkApi = checkPrefix('nest_conj-', NestCntDeclr, '_chi_nest_conj', handle);
+
+return checkApi;
 
 });
