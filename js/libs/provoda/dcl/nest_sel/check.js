8d5dc6f978c7ece596a4b36c1f7e0dfe962fafad
Gleb Arestov <gr1b0k@gmail.com>
2018-05-27 11:56:35 +0300

    Refactor nest_sel/check

--- js/libs/provoda/dcl/nest_sel/check.js
+++ js/libs/provoda/dcl/nest_sel/check.js
@@ -1,32 +1,17 @@
 define(function(require) {
 'use strict';
-var spv = require('spv');
-var getPropsPrefixChecker = require('../../utils/getPropsPrefixChecker');
-
-var getUnprefixed = spv.getDeprefixFunc( 'nest_sel-' );
-var hasPrefixedProps = getPropsPrefixChecker( getUnprefixed );
+var checkPrefix = require('../../StatesEmitter/checkPrefix');
 var SelectNestingDeclaration = require('./item')
 
-return function checkNestSel(self, props) {
-  if (!hasPrefixedProps(props)) {
-    return;
-  }
-
-  self._chi_nest_sel = spv.cloneObj({}, self._chi_nest_sel);
-
-  for (var name in props) {
-    var clean_name = getUnprefixed(name);
-    if (!clean_name) {
-      continue;
-    }
-
-    self._chi_nest_sel[name] = new SelectNestingDeclaration(clean_name, props[name]);
-  }
-
+var handle = function(self, _chi_nest_sel) {
   self.nest_sel_nest_matches = [];
 
-  for (var sel_res in self._chi_nest_sel) {
-    self.nest_sel_nest_matches.push(self._chi_nest_sel[sel_res]);
+  for (var sel_res in _chi_nest_sel) {
+    self.nest_sel_nest_matches.push(_chi_nest_sel[sel_res]);
   }
-};
+}
+
+var checkNestSel = checkPrefix('nest_sel-', SelectNestingDeclaration, '_chi_nest_sel', handle);
+
+return checkNestSel;
 });
