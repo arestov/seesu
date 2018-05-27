e9fe3221013ecf9158699b4e19178a07b589a552
Gleb Arestov <gr1b0k@gmail.com>
2018-05-27 12:06:39 +0300

    Refactor nest/check draftly

--- js/libs/provoda/dcl/nest/check.js
+++ js/libs/provoda/dcl/nest/check.js
@@ -10,13 +10,13 @@ return function(self, props) {
   var
     has_props = hasPrefixedProps(props),
     has_pack = self.hasOwnProperty('nest'),
-    prop, cur, real_name;
+    prop, real_name;
 
   if (!has_props && !has_pack) {
     return;
   }
 
-  var result = [];
+  var result = {};
 
   var used_props = {};
 
@@ -26,9 +26,9 @@ return function(self, props) {
       if (getUnprefixed(prop)) {
 
         real_name = getUnprefixed(prop);
-        cur = self[prop];
+        var cur = self[prop];
         used_props[real_name] = true;
-        result.push(new NestDcl(real_name, cur));
+        result[real_name] = new NestDcl(real_name, cur);
       }
     }
   }
@@ -38,19 +38,23 @@ return function(self, props) {
       if (used_props[real_name]) {
         continue;
       }
-      cur = self.nest[real_name];
+      var cur = self.nest[real_name];
       used_props[real_name] = true;
-      result.push(new NestDcl(real_name, cur));
+      result[real_name] = new NestDcl(real_name, cur);
     }
   }
 
-  self.nestings_declarations = result;
-  self.idx_nestings_declarations = {};
+  self.nestings_declarations = [];
+  self.idx_nestings_declarations = result;
   self._chi_nest = {};
-  for (var i = 0; i < result.length; i++) {
-    self.idx_nestings_declarations[result[i].nesting_name] = result[i];
 
-    var item = result[i].subpages_names_list;
+  for (var name in result) {
+    if (!result.hasOwnProperty(name)) {
+      continue;
+    }
+    var cur = result[name]
+    self.nestings_declarations.push(cur);
+    var item = cur.subpages_names_list;
     if (Array.isArray(item)) {
       for (var kk = 0; kk < item.length; kk++) {
         if (item[kk].type == 'constr') {
@@ -62,9 +66,6 @@ return function(self, props) {
         self._chi_nest[item.key] = item.value;
       }
     }
-
   }
-
-
 };
 });
