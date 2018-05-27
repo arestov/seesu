07c81b98dd9626672aa980c62a2ab76878d31a5e
Gleb Arestov <gr1b0k@gmail.com>
2018-05-27 12:07:58 +0300

    Create handle fn for nest/check

--- js/libs/provoda/dcl/nest/check.js
+++ js/libs/provoda/dcl/nest/check.js
@@ -6,6 +6,32 @@ var getUnprefixed = spv.getDeprefixFunc( 'nest-' );
 var hasPrefixedProps = getPropsPrefixChecker( getUnprefixed );
 var NestDcl = require('./item');
 
+var handle = function(self, result) {
+  self.nestings_declarations = [];
+  self.idx_nestings_declarations = result;
+  self._chi_nest = {};
+
+  for (var name in result) {
+    if (!result.hasOwnProperty(name)) {
+      continue;
+    }
+    var cur = result[name]
+    self.nestings_declarations.push(cur);
+    var item = cur.subpages_names_list;
+    if (Array.isArray(item)) {
+      for (var kk = 0; kk < item.length; kk++) {
+        if (item[kk].type == 'constr') {
+          self._chi_nest[item[kk].key] = item[kk].value;
+        }
+      }
+    } else {
+      if (item.type == 'constr') {
+        self._chi_nest[item.key] = item.value;
+      }
+    }
+  }
+}
+
 return function(self, props) {
   var
     has_props = hasPrefixedProps(props),
@@ -44,28 +70,6 @@ return function(self, props) {
     }
   }
 
-  self.nestings_declarations = [];
-  self.idx_nestings_declarations = result;
-  self._chi_nest = {};
-
-  for (var name in result) {
-    if (!result.hasOwnProperty(name)) {
-      continue;
-    }
-    var cur = result[name]
-    self.nestings_declarations.push(cur);
-    var item = cur.subpages_names_list;
-    if (Array.isArray(item)) {
-      for (var kk = 0; kk < item.length; kk++) {
-        if (item[kk].type == 'constr') {
-          self._chi_nest[item[kk].key] = item[kk].value;
-        }
-      }
-    } else {
-      if (item.type == 'constr') {
-        self._chi_nest[item.key] = item.value;
-      }
-    }
-  }
+  handle(self, result)
 };
 });
