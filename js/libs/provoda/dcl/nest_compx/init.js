588a1d4cf1ab8cd0500ccc04bb47409b2cceab98
Gleb Arestov <gr1b0k@gmail.com>
2018-08-18 16:35:59 +0300

    Remove comments

--- js/libs/provoda/dcl/nest_compx/init.js
+++ js/libs/provoda/dcl/nest_compx/init.js
@@ -1,10 +1,7 @@
 define(function(require) {
 'use strict';
 
-// var initWatchList = require('../../nest-watch/index').initList;
 var Runner = require('./runner')
-
-// var prsStCon = require('../../prsStCon');
 
 return function (self) {
   if (!self._nest_by_type_listed || !self._nest_by_type_listed.compx) {
@@ -15,16 +12,7 @@ return function (self) {
 
   for (var i = 0; i < compx_list.length; i++) {
     var cur = compx_list[i]
-    // debugger
     new Runner(self, cur);
-
-
-    // prsStCon.connect.parent(self, cur);
-    // prsStCon.connect.root(self, cur);
-    // prsStCon.connect.nesting(self, cur);
-    // debugger
-    // initWatchList(self, cur.compx_nest_matches)
-
   }
 }
 })
