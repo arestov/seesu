838ebfbf3a983f5392549ed61108ddc09b07ebea
Gleb Arestov <gr1b0k@gmail.com>
2018-08-18 15:53:31 +0300

    Separate bind in prsStCon

--- js/libs/provoda/prsStCon.js
+++ js/libs/provoda/prsStCon.js
@@ -1,7 +1,59 @@
 define(function(require) {
 'use strict';
+
+var bind = {
+  root: function(bind) {
+    return function(md, instructions) {
+      var list = instructions.conndst_root;
+      if (!list){
+        return;
+      }
+      for (var i = 0; i < list.length; i++) {
+        var cur = list[i];
+        var target = md.getStrucRoot();
+        if (!target){
+          throw new Error();
+        }
+
+        bind(md, target, cur.state_name, cur.full_name)
+      }
+    }
+  },
+  parent: function(bind) {
+    return function(md, instructions) {
+      var list = instructions.conndst_parent;
+      if (!list){
+        return;
+      }
+      for (var i = 0; i < list.length; i++) {
+        var cur = list[i];
+        var count = cur.ancestors;
+        var target = md;
+        while (count){
+          count--;
+          target = target.getStrucParent();
+        }
+        if (!target){
+          throw new Error();
+        }
+
+        bind(md, target, cur.state_name, cur.full_name)
+      }
+
+    }
+  }
+}
+
+var copyStates = function(md, target, state_name, full_name) {
+  md.wlch(target, state_name, full_name);
+}
+
   var pvState = require('./utils/state');
+
+
+
   return {
+    bind: bind,
     prefill: {
       parent: function (md, states_list) {
         var list = md.conndst_parent;
@@ -39,42 +91,9 @@ define(function(require) {
       }
     },
     connect: {
-      parent: function(md, instructions) {
-        var list = instructions.conndst_parent;
-        if (!list){
-          return;
-        }
-        for (var i = 0; i < list.length; i++) {
-          var cur = list[i];
-          var count = cur.ancestors;
-          var target = md;
-          while (count){
-            count--;
-            target = target.getStrucParent();
-          }
-          if (!target){
-            throw new Error();
-          }
-          md.wlch(target, cur.state_name, cur.full_name);
-        }
-
-      },
       nesting: function() {},
-      root: function(md, instructions) {
-        var list = instructions.conndst_root;
-        if (!list){
-          return;
-        }
-        for (var i = 0; i < list.length; i++) {
-          var cur = list[i];
-          var target = md.getStrucRoot();
-          if (!target){
-            throw new Error();
-          }
-          md.wlch(target, cur.state_name, cur.full_name);
-        }
-
-      }
+      parent: bind.parent(copyStates),
+      root: bind.root(copyStates),
     }
   };
 });
