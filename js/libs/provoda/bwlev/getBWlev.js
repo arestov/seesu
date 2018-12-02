4cb2290ae68f2131cdba450a838836b9d54880bf
Gleb Arestov <gr1b0k@gmail.com>
2018-12-02 10:49:04 +0300

    Refactor init bwlev

--- js/libs/provoda/bwlev/getBWlev.js
+++ js/libs/provoda/bwlev/getBWlev.js
@@ -2,6 +2,23 @@ define(function (require) {
 'use strict';
 var create = require('../create');
 
+var initBWlev = function(BrowseLevel, md, probe_name, map_level_num, map, parent_bwlev) {
+  var bwlev = create(BrowseLevel, {
+    probe_name: probe_name,
+    map_level_num: map_level_num,
+    // model_name: md.model_name,
+    pioneer_provoda_id: md._provoda_id,
+    pioneer: md
+  }, {
+    nestings: {
+      pioneer: md,
+      map: map
+    }
+  }, parent_bwlev, md.app);
+
+  return bwlev
+}
+
 return function getBWlev(BrowseLevel, md, probe_name, parent_bwlev, map_level_num, map){
   var cache = parent_bwlev && parent_bwlev.children_bwlevs;
   var key = md._provoda_id;
@@ -15,18 +32,7 @@ return function getBWlev(BrowseLevel, md, probe_name, parent_bwlev, map_level_nu
     throw new Error('provide BrowseLevel constructor');
   }
 
-  var bwlev = create(BrowseLevel, {
-    probe_name: probe_name,
-    map_level_num: map_level_num,
-    // model_name: md.model_name,
-    pioneer_provoda_id: md._provoda_id,
-    pioneer: md
-  }, {
-    nestings: {
-      pioneer: md,
-      map: map
-    }
-  }, parent_bwlev, md.app);
+  var bwlev = initBWlev(BrowseLevel, md, probe_name, map_level_num, map, parent_bwlev)
 
   if (cache) {
     cache[key] = bwlev;
