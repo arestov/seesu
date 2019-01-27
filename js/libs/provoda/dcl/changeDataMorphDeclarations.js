4be07298a3e394a8f615d1b17f47ddfdba5f39c3
Gleb Arestov <gr1b0k@gmail.com>
2019-01-27 10:14:43 +0300

    Remove useless code

--- js/libs/provoda/dcl/changeDataMorphDeclarations.js
+++ js/libs/provoda/dcl/changeDataMorphDeclarations.js
@@ -1,11 +1,9 @@
 define(function(require){
 'use strict';
-var assign = require('./effects/legacy/utils/assign')
-
 var StateReqMap = require('./effects/legacy/state_req/dcl')
 var buildStateReqs = require('./effects/legacy/state_req/rebuild')
 
-return function(self, props, typed_state_dcls) {
+return function(self, props) {
   if (props.hasOwnProperty('req_map')) {
     var list = new Array(props.req_map.length);
     for (var i = 0; i < props.req_map.length; i++) {
@@ -13,13 +11,6 @@ return function(self, props, typed_state_dcls) {
     }
 
     buildStateReqs(self, list)
-  }
-
-  var main_list_nest_req = self.main_list_nest_req;
-
-  if (props.hasOwnProperty('main_list_nest_req') && main_list_nest_req && main_list_nest_req.nest_name !== props.main_list_name) {
-    assign(typed_state_dcls, main_list_nest_req.original);
-    self._nest_reqs[main_list_nest_req.nest_name] = main_list_nest_req.original;
   }
 };
 
