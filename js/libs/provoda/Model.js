f951dd537011c06b452b30fe25661cc2df7168d0
Gleb Arestov <gr1b0k@gmail.com>
2019-03-10 14:55:02 +0300

    Move initSi

--- js/libs/provoda/Model.js
+++ js/libs/provoda/Model.js
@@ -14,6 +14,7 @@ var onPropsExtend = require('./Model/onExtend');
 var initModel = require('./Model/init');
 var updateNesting = require('./Model/updateNesting');
 var postInitModel = require('./Model/postInit')
+var initSi = require('./Model/initConstr/subitem')
 
 var push = Array.prototype.push;
 var cloneObj = spv.cloneObj;
@@ -68,39 +69,6 @@ var changeSourcesByApiNames = function(md, store) {
     }
   }
 };
-
-function initSi(Constr, parent_md, data, params, more, states) {
-  if (Constr.prototype.conndst_parent && Constr.prototype.conndst_parent.length) {
-    if (Constr.prototype.pconstr_id !== true && parent_md.constr_id !== Constr.prototype.pconstr_id) {
-      console.log( (new Error('pconstr_id should match constr_id')).stack );
-    }
-  }
-
-  if (Constr.prototype.init) {
-    var instance = new Constr();
-    var initsbi_opts = parent_md.getSiOpts();
-
-    parent_md.useMotivator(instance, function(instance) {
-      instance.init(initsbi_opts, data, params, more, states);
-    });
-
-    return instance;
-  } else {
-    var motivator = parent_md.current_motivator;
-
-    var opts = {
-      _motivator: motivator,
-      map_parent: parent_md != parent_md.app && parent_md,
-      app: parent_md.app
-    };
-
-    var instancePure = new Constr(opts, data, params, more, states);
-
-    instancePure.current_motivator = null;
-
-    return instancePure;
-  }
-}
 
 var Model = spv.inh(StatesEmitter, {
   naming: function(fn) {
---
+++ js/libs/provoda/Model/initConstr/subitem.js
@@ -0,0 +1,36 @@
+define(function() {
+'use strict'
+
+return function initSi(Constr, parent_md, data, params, more, states) {
+  if (Constr.prototype.conndst_parent && Constr.prototype.conndst_parent.length) {
+    if (Constr.prototype.pconstr_id !== true && parent_md.constr_id !== Constr.prototype.pconstr_id) {
+      console.log( (new Error('pconstr_id should match constr_id')).stack );
+    }
+  }
+
+  if (Constr.prototype.init) {
+    var instance = new Constr();
+    var initsbi_opts = parent_md.getSiOpts();
+
+    parent_md.useMotivator(instance, function(instance) {
+      instance.init(initsbi_opts, data, params, more, states);
+    });
+
+    return instance;
+  } else {
+    var motivator = parent_md.current_motivator;
+
+    var opts = {
+      _motivator: motivator,
+      map_parent: parent_md != parent_md.app && parent_md,
+      app: parent_md.app
+    };
+
+    var instancePure = new Constr(opts, data, params, more, states);
+
+    instancePure.current_motivator = null;
+
+    return instancePure;
+  }
+}
+})
