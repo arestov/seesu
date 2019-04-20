ca2b74352be4a2014d632e7753b7d7bf646da9e9
Gleb Arestov <gr1b0k@gmail.com>
2019-04-20 17:34:49 +0300

    DRF Move initBrowsing

--- test/init.js
+++ test/init.js
@@ -1,9 +1,5 @@
 define(require => {
   // app thread;
-  const BrowseMap = require('js/libs/BrowseMap')
-  const pv = require('pv')
-  const animateMapChanges = require('js/libs/provoda/dcl/probe/animateMapChanges')
-
   const fakeApp = require('./fakeApp')
   const initApp = require('js/utils/initApp')
 
@@ -13,31 +9,5 @@ define(require => {
   return function init(app_props, init) {
     const App = fakeApp(app_props, init)
     return initApp(App, env)
-  }
-
-  function initBrowsing(app) {
-    const map = BrowseMap.hookRoot(app, app.start_page)
-    app.map = map
-
-    initMapTree(app, app.start_page)
-
-    const bwlev = BrowseMap.showInterest(map, [])
-    BrowseMap.changeBridge(bwlev)
-
-    return map
-  }
-
-
-  function initMapTree(app, start_page) {
-  // app.useInterface('navi', needs_url_history && navi);
-    pv.updateNesting(app, 'navigation', [])
-    pv.updateNesting(app, 'start_page', start_page)
-
-    app.map
-      .on('bridge-changed', bwlev => {
-        animateMapChanges(app, bwlev)
-      }, app.getContextOptsI())
-
-    return app.map
   }
 })
