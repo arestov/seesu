54345b203673b1ff8958b59fe493126e00a9ca13
Gleb Arestov <gr1b0k@gmail.com>
2019-04-24 19:35:32 +0300

    Fix using app_env

--- js/app/effects.js
+++ js/app/effects.js
@@ -1,21 +1,26 @@
 define(function(require) {
 'use strict'
 // var spv = require('spv');
+var app_serv = require('app_serv');
+var app_env = app_serv.app_env;
 var subscribeLfmAuthAction = require('./subscribeLfmAuthAction')
 
 return {
   api: {
     "window": function() {
       return window;
+    },
+    "app_env": function() {
+      return app_env
     }
   },
   produce: {
     "lfm_auth_subscribe": {
-      api: ["self", "window"],
-      trigger: ["lfm_auth$exists", "env"],
+      api: ["self", "window", "app_env"],
+      trigger: ["lfm_auth$exists"],
       require: "lfm_auth$exists",
-      fn: function(self, win, _, env) {
-        subscribeLfmAuthAction(self, win, env)
+      fn: function(self, win, app_env) {
+        subscribeLfmAuthAction(self, win, app_env)
       }
     },
   }
