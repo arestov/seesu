45375f6b2237d36db6421214e42c86cd1455f2f7
Gleb Arestov <gr1b0k@gmail.com>
2019-06-28 17:00:11 +0300

    Create test for multiPath/parse

---
+++ js/libs/provoda/utils/multiPath/parse.test.js
@@ -0,0 +1,48 @@
+var test = require('ava')
+var requirejs = require('requirejs');
+var path = require('path')
+requirejs.config({
+  baseUrl: __dirname,
+  map: {
+		'*': {
+			spv: path.join(process.cwd(), 'js/libs/spv.js'),
+		}
+	},
+})
+var parse = requirejs('./parse')
+test('check parsing', function(t) {
+  'use strict'
+
+
+  //
+  // "< state_name < nesting < resource < #"
+
+
+  t.snapshot(parse("< state_name < nesting < resource < #"));
+
+  t.throws(function() {
+    parse("< state_name < aggr:nesting < resource < #")
+  });
+
+  t.snapshot(parse("< state_name << /resource/[:ddaf]/sdf < #"));
+
+  t.snapshot(parse("< state_name <<< #"));
+
+  t.snapshot(parse("<< nesting_name << #"));
+
+  t.snapshot(parse("<< nesting_name << ^^"));
+
+  t.snapshot(parse("< state_name <<< ^^"));
+
+  t.snapshot(parse("< state_name"));
+
+  t.snapshot(parse("state_name"));
+
+  t.snapshot(parse("/resource/[:ddaf]/sdf < #"));
+
+  t.snapshot(parse("/resource/[:ddaf]/sdf <"));
+
+  t.snapshot(parse("nesting_name < < ^^"));
+
+
+});
