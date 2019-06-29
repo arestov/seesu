abeb46f1cdbaea103dfb2337853bd4e6f807de2a
Gleb Arestov <gr1b0k@gmail.com>
2019-06-29 13:27:05 +0300

    Handle zipping in nest compx

--- js/libs/provoda/dcl/nest_compx/handler.js
+++ js/libs/provoda/dcl/nest_compx/handler.js
@@ -4,6 +4,7 @@ define(function(require) {
 var updateNesting = require('../../Model/updateNesting');
 var multiPathAsString = require('../../utils/multiPath/asString')
 var isNestingChanged = require('../../utils/isNestingChanged')
+var pvState = require('pv/state')
 
 var prepareArgs = function(dcl, _runStates) {
   var result = new Array(dcl.deps.length);
@@ -66,16 +67,72 @@ var changeValue = function(runner, dep_full_name, value, checkFromNesting) {
   recalc(dcl, runner)
 }
 
+var getOneValue = function(dep, item) {
+  if (!item) {
+    return item;
+  }
+
+  if (dep.result_type != 'state') {
+    return item
+  }
+
+  return pvState(item, dep.state.base)
+}
+
+var mapList = function(dep, list) {
+  var result = new Array(list.length)
+  for (var i = 0; i < list.length; i++) {
+    result[i] = getOneValue(dep, list[i])
+  }
+  return result
+}
+
+var empty = ''
+var isMeaningfulValue = function(value) {
+  // https://twitter.com/jonathoda/status/960952613507231744
+  return value != null && value !== empty;
+}
+
+var zipValue = function(runner, lwroot, list) {
+  var dep = lwroot.data.dep
+
+  switch (dep.zip_name) {
+    case "one": {
+      return list && getOneValue(dep, list[0])
+    }
+    case "every": {
+      return list && mapList(dep, list).every(isMeaningfulValue)
+    }
+    case "some": {
+      return list && mapList(dep, list).some(isMeaningfulValue)
+    }
+    case "find": {
+      return list && mapList(dep, list).find(isMeaningfulValue)
+    }
+    case "all":
+    default: {
+      return list && mapList(dep, list)
+    }
+  }
+}
+
+var getValue = function(runner, lwroot, list) {
+  if (!lwroot.state_name && !lwroot.data.dep.zip_name) {
+    // we can get same, but mutated `value`
+    var to_pass = Array.isArray(list) ? list.slice(0) : list;
+    return to_pass
+  }
+
+  return zipValue(runner, lwroot, list)
+}
+
 
 return {
   hnest: function nestCompxNestDepChangeHandler(flow_step, _, lwroot, __, value) {
     var data = lwroot.data
     var runner = data.runner
 
-    // we can get same, but mutated `value`
-    var to_pass = Array.isArray(value) ? value.slice(0) : value;
-
-    changeValue(runner, multiPathAsString(data.dep), to_pass, true)
+    changeValue(runner, multiPathAsString(data.dep), getValue(runner, lwroot, value), true)
   },
   hstate: function nestCompxStateDepChangeHandler(runner, dep_full_name, value) {
     changeValue(runner, dep_full_name, value, false)
