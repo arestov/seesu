f6bb1b5d0ca91950a9a7860988b6318355adb89d
Gleb Arestov <gr1b0k@gmail.com>
2019-06-29 13:32:14 +0300

    Add comment

--- js/libs/provoda/dcl/nest_compx/handler.js
+++ js/libs/provoda/dcl/nest_compx/handler.js
@@ -90,6 +90,7 @@ var mapList = function(dep, list) {
 var empty = ''
 var isMeaningfulValue = function(value) {
   // https://twitter.com/jonathoda/status/960952613507231744
+  // https://twitter.com/jonathoda/status/1138881930399703041
   return value != null && value !== empty;
 }
 
