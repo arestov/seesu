4840ef615bd2ce484a8f596c8373b5a571b3451a
Gleb Arestov <gr1b0k@gmail.com>
2019-09-15 10:06:42 +0300

    Improve getModel

--- js/libs/provoda/View/getModel.js
+++ js/libs/provoda/View/getModel.js
@@ -2,13 +2,13 @@ define(function() {
 'use strict';
 
 return function getModel(view, _provoda_id) {
-  var proxies_space = view.proxies_space || view.root_view.proxies_space;
-  if (view._highway.views_proxies) {
-    var mpx = view._highway.views_proxies.spaces[proxies_space].mpxes_index[_provoda_id]
-    return mpx.md;
+  if (!view._highway.views_proxies) {
+    return view._highway.sync_r.models_index[_provoda_id]
   }
 
-  return view._highway.sync_r.models_index[_provoda_id]
+  var proxies_space = view.proxies_space || view.root_view.proxies_space;
+  var mpx = view._highway.views_proxies.spaces[proxies_space].mpxes_index[_provoda_id]
+  return mpx.md;
 }
 
 })
