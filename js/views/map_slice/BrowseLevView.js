75cab577f1d4eef49d0b3d6e1abd8d9e508eb3eb
Gleb Arestov <gr1b0k@gmail.com>
2019-10-27 09:59:12 +0300

    Fix BrowseLevView

--- js/views/map_slice/BrowseLevView.js
+++ js/views/map_slice/BrowseLevView.js
@@ -4,7 +4,7 @@ var spv = require('spv');
 var map_slice_by_model = require('./pages/index');
 var BrowseLevView = require('./BrowseLevViewCore')
 
-var BrowseLevView = spv.inh({}, {}, {
+var BrowseLevView = spv.inh(BrowseLevView, {}, {
   children_views_by_mn: {
     pioneer: map_slice_by_model
   },
